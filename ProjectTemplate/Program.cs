c54ef74100ea1c5a9f6fc310081ac95f6cb98051
Dave G <34277803+SoloByte@users.noreply.github.com>
2023-01-09 08:17:55 +0100

    project template update.

--- ProjectTemplate/Program.cs
+++ ProjectTemplate/Program.cs
@@ -1,7 +1,7 @@
 ﻿global using static Raylib_CsLo.Raylib;
 global using static Raylib_CsLo.RayMath;
-global using static ShapeEngineCore.ShapeEngine;
-using ShapeEngineCore;
+global using static ShapeCore.ShapeEngine;
+using ShapeCore;
 
 
 //either copy into project file or download those 2 nuget packages.
@@ -20,7 +20,7 @@ namespace ShapeEngineTemplate
         {
             //ShapeEngineCore.Globals.Persistent.ResourceManager.Generate("resources", "", "resources.txt");
 
-            ScreenInitInfo screenInitInfo = new ScreenInitInfo(1920, 1080, 1f, 1.0f, "Shape Engine Template", 60, true, false, 0, false);
+            ScreenInitInfo screenInitInfo = new ScreenInitInfo(1920, 1080, 1f, 1.0f, "Shape Engine Template", false, false);
             ResourceInitInfo resourceInitInfo = new("", "resources.txt");
             GameInitInfo gameInitInfo = new("solobytegames", "shape-engine-demo");
             ShapeEngine.Start(new GameLoop(), gameInitInfo, resourceInitInfo, screenInitInfo);
