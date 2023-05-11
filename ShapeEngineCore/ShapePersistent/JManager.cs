7a9a6f28751955cbb1cbb70dadd153804bd4490e
Dave G <34277803+SoloByte@users.noreply.github.com>
2023-05-11 16:52:22 +0200

    new json data sytem improvements.

--- ShapeEngineCore/ShapePersistent/JManager.cs
+++ ShapeEngineCore/ShapePersistent/JManager.cs
@@ -5,12 +5,20 @@ using System.Text.Json;
 
 namespace ShapePersistent
 {
+    public interface IDataContainer
+    {
+        public IDataObject GetRandom();
+    }
+    public interface IDataObject
+    {
+        public string GetName();
+        
+    }
 
     public sealed class JNode
     {
         private JsonObject node;
         public bool Valid { get; private set; } = false;
-
 
         public JNode() { this.node = new JsonObject(); }
         public JNode(string json)
@@ -37,6 +45,36 @@ namespace ShapePersistent
         public string GetJsonString() { return node.ToJsonString(); }
         public bool ContainsKey(string key) { return Valid && node.ContainsKey(key); }
 
+        public JNode GetNode(string key)
+        {
+            if (node.ContainsKey(key))
+            {
+                var n = node[key];
+                if(n != null) return new JNode(n);
+            }
+            return new JNode();
+        }
+        public JNode[] GetArray(string key)
+        {
+            if (!Valid) return Array.Empty<JNode>();   
+            if(node.ContainsKey(key))
+            {
+                var arr = node[key]?.AsArray();
+                if(arr != null)
+                {
+                    
+                    List<JNode> result = new();
+                    foreach (var item in arr)
+                    {
+                        var jn = new JNode(item);
+                        if (jn.Valid) result.Add(jn);
+                    }
+                    return result.ToArray();
+                }
+            }
+            return Array.Empty<JNode>();
+        }
+        
         public T? Deserialize<T>() { return node.Deserialize<T>(); }
         public T? GetProperty<T>(string key)
         {
@@ -47,117 +85,67 @@ namespace ShapePersistent
             }
             return default(T);
         }
-        public JNode GetNode(string key)
+        
+        public List<T> GetSheet<T>(string key)
         {
+            List<T> result = new();
+
             if (node.ContainsKey(key))
             {
-                var n = node[key];
-                if(n != null) return new JNode(n);
+                var arr = node[key]?.AsArray();
+                if (arr != null)
+                {
+                    foreach (var item in arr)
+                    {
+                        if (item == null) continue;
+
+                        var obj = item.AsObject();
+                        if (obj != null)
+                        {
+                            var t = obj.Deserialize<T>();
+                            if (t != null) result.Add(t);
+                        }
+                    }
+                }
             }
-            return new JNode();
+            return result;
         }
-        public HashSet<JNode> GetArray(string key)
+        public Dictionary<string, T> GetSheet<T>(string key, string idPropertyName)
         {
-            if (!Valid) return new();   
-            if(node.ContainsKey(key))
+            Dictionary<string, T> result = new();
+
+            if (node.ContainsKey(key))
             {
                 var arr = node[key]?.AsArray();
                 if(arr != null)
                 {
-                    HashSet<JNode> result = new();
                     foreach (var item in arr)
                     {
-                        var jn = new JNode(item);
-                        if (jn.Valid) result.Add(jn);
-                    }
-                    return result;
-                }
-            }
-            return new();
-        }
-        
-    }
-
-    //public class JArray
-    //{
-    //    private HashSet<JNode> entries = new();
-    //    public bool Valid { get; private set; } = false;
-    //    
-    //    public JArray() { }
-    //    internal JArray(JsonNode node)
-    //    {
-    //        var arr = node.AsArray();
-    //        foreach(var item in arr)
-    //        {
-    //            var jn = new JNode(item);
-    //            if(jn.Valid) entries.Add(jn);
-    //        }
-    //        if(entries.Count > 0) Valid = true;
-    //    }
-    //
-    //
-    //
-    //    //private JsonArray arr;
-    //    //public bool Valid { get; private set; } = false;
-    //    //
-    //    //public JArray() { arr = new JsonArray(); }
-    //    //internal JArray(JsonNode node)
-    //    //{
-    //    //    arr = node.AsArray();
-    //    //    Valid = true;
-    //    //}
-    //    //
-    //    //public JNode Get(string property, string value)
-    //    //{
-    //    //
-    //    //}
-    //}
-
-    //public class JNode
-    //{
-    //    private JsonNode node;
-    //    private JNode(JsonNode node)
-    //    {
-    //        this.node = node;
-    //    }
-    //
-    //    public static JNode? Create(string json)
-    //    {
-    //        var dataNode = JsonNode.Parse(json);
-    //        if (dataNode == null) return null;
-    //        else return new JNode(dataNode);
-    //    }
-    //}
-    
-    public class Player { }
-    public class JManager
-    {
-
-        public JManager()
-        {
-            string json = "asoihfdahsfhkjo";
-            var node = new JNode(json);
-            var arr = node.GetArray("sheets");
-            foreach (var item in arr)
-            {
-                if(item.ContainsKey("name") && item.GetProperty<string>("name") == "player")
-                {
-                    var lines = item.GetArray("lines");
-                    Dictionary<string, Player> playerData = new();
-                    foreach (var line in lines)
-                    {
-                        if (line.ContainsKey("name"))
+                        if(item == null) continue;
+                        
+                        var obj = item.AsObject();
+                        if(obj != null)
                         {
-                            var name = line.GetProperty<string>("name");
-                            var player = line.Deserialize<Player>();
-                            if(player != null && name != null)
+                            if (obj.ContainsKey(idPropertyName))
                             {
-                                playerData.Add(name, player);
+                                var id = obj[idPropertyName]?.ToString();
+                                var t = obj.Deserialize<T>();
+                                if(id != null && t != null) result.Add(id, t);
                             }
                         }
                     }
                 }
             }
+            return result;
+        }
+    }
+   
+    public class JManager
+    {
+
+        public JManager()
+        {
+            
         }
 
 
