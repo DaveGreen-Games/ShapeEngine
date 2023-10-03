0f91928ab3fbe7642d58b06d903c454684572838
Dave Green <34277803+SoloByte@users.noreply.github.com>
2023-10-03 21:41:44 +0200

    new slow system started.

--- ShapeEngine/Core/ShapeLoop.cs
+++ ShapeEngine/Core/ShapeLoop.cs
@@ -12,6 +12,145 @@ using ShapeEngine.Core.Shapes;
 
 namespace ShapeEngine.Core;
 
+
+public sealed class SlowMotion
+{
+    private readonly Dictionary<uint, SlowItemContainer> slowItemContainers = new();
+
+    public void Update(float dt)
+    {
+        foreach (var container in slowItemContainers.Values)
+        {
+            container.Update(dt);
+        }
+    }
+    
+    public uint Add(float factor, float duration, uint tag)
+    {
+        var slowItem = new SlowItem(factor, duration, tag);
+        
+        if(slowItemContainers.ContainsKey(tag)) slowItemContainers[tag].Add(slowItem);
+        else slowItemContainers.Add(tag, new SlowItemContainer(){slowItem});
+
+        return slowItem.ID;
+    }
+
+    public void Clear() => slowItemContainers.Clear();
+    public bool RemoveTag(uint tag) => slowItemContainers.Remove(tag);
+    public void Remove(uint id, uint tag)
+    {
+        if (!slowItemContainers.ContainsKey(tag)) return;
+    }
+    public bool Remove(uint id)
+    {
+        foreach (var container in slowItemContainers.Values)
+        {
+            if (container.RemoveID(id)) return true;
+        }
+
+        return false;
+    }
+    public bool HasTag(uint tag) => slowItemContainers.ContainsKey(tag);
+    public float GetFactor(uint tag)
+    {
+        if (!HasTag(tag)) return 1f;
+        return slowItemContainers[tag].TotalFactor;
+    }
+    public float GetFactor(params uint[] tags)
+    {
+        float totalFactor = 1f;
+        foreach (var tag in tags)
+        {
+            totalFactor *= GetFactor(tag);
+        }
+
+        return totalFactor;
+    }
+    
+}
+internal class SlowItemContainer : List<SlowItem>
+{
+    public float TotalFactor { get; private set; } = 1f;
+    
+    public void Update(float dt)
+    {
+        float totalFactor = 1f;
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            var item = this[i];
+            item.Update(dt);
+            if (item.Finished) RemoveAt(i);
+            else totalFactor *= item.Factor;
+
+        }
+
+        TotalFactor = totalFactor;
+    }
+
+    public bool RemoveID(uint id)
+    {
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            if (this[i].ID == id)
+            {
+                RemoveAt(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+internal class SlowItem
+{
+    public uint ID { get; private set; }
+    public uint Tag { get; private set; }
+    public float Factor { get; private set; }
+
+    private float timer = 0f;
+
+    public SlowItem(float factor, float duration, uint tag)
+    {
+        ID = ShapeID.NextID;
+        Tag = tag;
+        Factor = factor;
+        if (duration > 0f)
+        {
+            // this.duration = duration;
+            this.timer = duration;
+        }
+    }
+    private SlowItem(float factor, float duration, uint tag, uint id)
+    {
+        ID = id;
+        Tag = tag;
+        Factor = factor;
+        if (duration > 0f)
+        {
+            // this.duration = duration;
+            this.timer = duration;
+        }
+    }
+
+    public SlowItem Copy()
+    {
+        var copy = new SlowItem(Factor, timer, Tag, ID);
+        return copy;
+    }
+
+    
+    public virtual bool Finished => timer < 0f;
+    public virtual void Update(float dt)
+    {
+        if (timer <= 0f) return;
+
+        timer -= dt;
+        if (timer <= 0f) timer = -1f;
+    }
+}
+
+
+
 public class ShapeLoop
 {
     #region Static
@@ -65,6 +204,7 @@ public class ShapeLoop
     public ScreenInfo Game { get; private set; } = new();
     public ScreenInfo UI { get; private set; } = new();
     public float Delta { get; private set; } = 0f;
+    // public float DeltaSlowFactor { get; private set; } = 1f;
     public bool ScreenShaderAffectsUI { get; set; } = false;
     public MonitorDevice Monitor { get; private set; }
     public ICursor Cursor { get; private set; } = new NullCursor();
