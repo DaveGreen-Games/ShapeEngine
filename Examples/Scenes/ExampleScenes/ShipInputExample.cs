a12194e7db980714f515ffb5b71242ead5e7b8d7
Dave Green <34277803+SoloByte@users.noreply.github.com>
2023-11-23 09:05:13 +0100

    ship input example improvements.

--- Examples/Scenes/ExampleScenes/ShipInputExample.cs
+++ Examples/Scenes/ExampleScenes/ShipInputExample.cs
@@ -134,7 +134,41 @@ namespace Examples.Scenes.ExampleScenes
         internal class CameraFollower : ICameraFollowTarget
         {
             private readonly List<IFollowTarget> targets = new();
-
+            private readonly ShapeCamera camera;
+            private Vector2 followPosition = new();
+            public CameraFollower()
+            {
+                camera = new()
+                {
+                    Follower =
+                    {
+                        FollowSpeed = SpaceShip.Speed * 2f
+                    }
+                };
+            }
+            public Vector2 GetRandomPosition()
+            {
+                return camera.Area.GetRandomPointInside();
+            }
+            
+            public void Reset()
+            {
+                camera.Reset();
+                camera.Follower.SetTarget(this);
+                SetCameraValues();
+            }
+            public void Activate()
+            {
+                GAMELOOP.Camera = camera;
+                camera.Follower.SetTarget(this);
+                SetCameraValues();
+            }
+            public void Deactivate()
+            {
+                GAMELOOP.ResetCamera();
+            }
+            
+            
             public bool AddTarget(IFollowTarget newTarget)
             {
                 if (targets.Contains(newTarget)) return false;
@@ -149,9 +183,7 @@ namespace Examples.Scenes.ExampleScenes
 
             public void Update(float dt)
             {
-                //set zoom to fit all ships
-                //set follow boundary correctly
-                //update position (cache)
+                SetCameraValues();
             }
             public void FollowStarted()
             {
@@ -159,25 +191,40 @@ namespace Examples.Scenes.ExampleScenes
             }
             public void FollowEnded()
             {
-                targets.Clear();
+                
             }
             public Vector2 GetCameraFollowPosition()
             {
-                return GetAveragePosition();
+                return followPosition;
+            }
+            private void UpdateCameraFollowBoundary()
+            {
+                var rect = camera.Area;
+                float size = rect.Size.Min();
+                var boundary = new Vector2(size * 0.15f, size * 0.4f);
+                camera.Follower.BoundaryDis = new(boundary);
             }
 
-            private Vector2 GetAveragePosition()
+            private void SetCameraValues()
             {
-                if (targets.Count <= 0) return new();
-                else if (targets.Count == 1) return targets[0].GetPosition();
-                
+                var cameraArea = camera.Area;
                 var totalPosition = new Vector2();
+                var newCameraRect = new Rect(cameraArea.Center, new(), new(0.5f));
+                
                 foreach (var target in targets)
                 {
-                    totalPosition += target.GetPosition();
+                    var pos = target.GetPosition();
+                    totalPosition += pos;
+                    newCameraRect = newCameraRect.Enlarge(pos);
                 }
 
-                return totalPosition / targets.Count;
+                var curSize = cameraArea.Size;
+                var newSize = newCameraRect.Size;
+                float f = 1f - (newSize.GetArea() / curSize.GetArea());
+                camera.SetZoom(f);
+                followPosition = totalPosition / targets.Count;
+                
+                UpdateCameraFollowBoundary();
             }
         }
         
@@ -185,12 +232,11 @@ namespace Examples.Scenes.ExampleScenes
         private readonly Rect universe = new(new Vector2(0f), new Vector2(10000f), new Vector2(0.5f));
         private readonly List<Star> stars = new();
 
-        private readonly CameraFollower cameraFollower = new();
+        private readonly CameraFollower cameraHandler = new();
         private readonly List<SpaceShip> spaceShips = new();
+        private bool setupFinished = false;
 
         
-        private readonly ShapeCamera camera = new ShapeCamera();
-        private bool setupFinished = false;
         public ShipInputExample()
         {
             Title = "Ship Input Example";
@@ -199,16 +245,12 @@ namespace Examples.Scenes.ExampleScenes
                 
             GenerateStars(2500);
 
-            UpdateCameraFollowBoundary(GAMELOOP.UI.Area.Size.Min());
-            
-            camera.Follower.FollowSpeed = SpaceShip.Speed * 2f;
-
             var spaceShip1 = new SpaceShip(new Vector2(), 30f, BLUE, PURPLE, LIGHTGRAY, true);
             var spaceShip2 = new SpaceShip(new Vector2(), 30f, GREEN, LIME, LIGHTGRAY);
             spaceShips.Add(spaceShip1);
             spaceShips.Add(spaceShip2);
-            cameraFollower.AddTarget(spaceShip1);
-            cameraFollower.AddTarget(spaceShip2);
+            cameraHandler.AddTarget(spaceShip1);
+            cameraHandler.AddTarget(spaceShip2);
         }
         private void GenerateStars(int amount)
         {
@@ -225,24 +267,23 @@ namespace Examples.Scenes.ExampleScenes
         
         public override void Activate(IScene oldScene)
         {
-            GAMELOOP.Camera = camera;
-            UpdateCameraFollowBoundary(GAMELOOP.UI.Area.Size.Min());
-            camera.Follower.SetTarget(cameraFollower);
-
             if (!setupFinished)
             {
                 foreach (var ship in spaceShips)
                 {
-                    ship.Reset(GetRandomCameraAreaPosition(), 30f);
+                    ship.Reset(cameraHandler.GetRandomPosition(), 30f);
                 }
 
                 setupFinished = true;
             }
+            
+            cameraHandler.Activate();
         }
 
         public override void Deactivate()
         {
-            GAMELOOP.ResetCamera();
+            cameraHandler.Deactivate();
+            
         }
         public override GameObjectHandler? GetGameObjectHandler()
         {
@@ -251,21 +292,18 @@ namespace Examples.Scenes.ExampleScenes
         public override void Reset()
         {
             GAMELOOP.ScreenEffectIntensity = 1f;
-            camera.Reset();
+            
             foreach (var ship in spaceShips)
             {
-                ship.Reset(GetRandomCameraAreaPosition(), 30f);
+                ship.Reset(cameraHandler.GetRandomPosition(), 30f);
             }
-            UpdateCameraFollowBoundary(GAMELOOP.UI.Area.Size.Min());
-            camera.Follower.SetTarget(cameraFollower);
             stars.Clear();
             GenerateStars(2500);
+            
+            cameraHandler.Reset();
         }
 
-        private Vector2 GetRandomCameraAreaPosition()
-        {
-            return camera.Area.GetRandomPointInside();
-        }
+        
         // private void ShakeCamera()
         // {
         //     camera.Shake(ShapeRandom.randF(0.8f, 2f), new Vector2(100, 100), 0, 25, 0.75f);
@@ -295,15 +333,10 @@ namespace Examples.Scenes.ExampleScenes
 
         }
 
-        private void UpdateCameraFollowBoundary(float size)
-        {
-            var boundary = new Vector2(size * 0.15f, size * 0.4f) * camera.ZoomFactor;
-            camera.Follower.BoundaryDis = new(boundary);
-        }
+        
         protected override void UpdateExample(float dt, float deltaSlow, ScreenInfo game, ScreenInfo ui)
         {
-            UpdateCameraFollowBoundary(ui.Area.Size.Min());
-
+            cameraHandler.Update(dt);
             foreach (var ship in spaceShips)
             {
                 ship.Update(dt);
