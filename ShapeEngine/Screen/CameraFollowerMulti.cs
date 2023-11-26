d51e24d71606d805150e5c3e9a1fbb3e9a29af69
Dave Green <34277803+SoloByte@users.noreply.github.com>
2023-11-26 19:31:44 +0100

    CameraFollowerSingle & CameraFollowerMulti moved to their own files.

---
+++ ShapeEngine/Screen/CameraFollowerMulti.cs
@@ -0,0 +1,25 @@
+namespace ShapeEngine.Screen;
+
+public class CameraFollowerMulti : ICameraFollower
+{
+    public CameraFollowerMulti()
+    {
+        
+        
+    }
+    
+    public void Reset()
+    {
+        
+    }
+
+    public void Update(float dt, ShapeCamera camera)
+    {
+        
+    }
+    
+
+    public void OnCameraAttached(ShapeCamera camera) { }
+
+    public void OnCameraDetached(ShapeCamera camera) { }
+}
