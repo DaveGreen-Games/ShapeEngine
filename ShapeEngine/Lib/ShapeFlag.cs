58ef5f5383c6493582d50ff41d13c1f82b6a373a
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-02-13 11:09:50 +0100

    bitflag struct implemented & static ShapeFlag class removed.

--- ShapeEngine/Lib/ShapeFlag.cs
+++ ShapeEngine/Lib/ShapeFlag.cs
@@ -5,12 +5,14 @@ namespace ShapeEngine.Lib;
 public readonly struct BitFlag : IBitwiseOperators<BitFlag, BitFlag, BitFlag>, IBitwiseOperators<BitFlag, uint, BitFlag>
 {
     public readonly uint Flag;
-
     public BitFlag(uint value)
     {
         Flag = value;
     }
-    public BitFlag Clear() => new(0);
+    
+    #region Operations Uint
+    public BitFlag Clear() => Empty;
+    public bool IsEmpty() => Flag == 0;
     public bool Has(uint value)
     {
         if (Flag == 0) return false;
@@ -49,7 +51,138 @@ public readonly struct BitFlag : IBitwiseOperators<BitFlag, BitFlag, BitFlag>, I
 
         return new(flag);
     }
+    #endregion
+    
+    #region Operation BitFlag
+    public bool Has(BitFlag value)
+    {
+        if (Flag == 0) return false;
+        return (Flag & value.Flag) != 0;
+    }
+    public BitFlag Set(BitFlag value)
+    {
+        return new(Flag ^ value.Flag);
+    }
+    public BitFlag Add(BitFlag value)
+    {
+        return new(Flag | value.Flag);
+    }
+    public BitFlag Remove(BitFlag value)
+    {
+        return new(Flag & ~value.Flag);
+    }
+    public BitFlag Add(params BitFlag[] values)
+    {
+        var flag = Flag;
+        foreach (var v in values)
+        {
+            flag |= v.Flag;
+        }
 
+        return new(flag);
+    }
+    public BitFlag Remove(params BitFlag[] values)
+    {
+        var flag = Flag;
+        foreach (var v in values)
+        {
+            flag &= ~v.Flag;
+        }
+
+        return new(flag);
+    }
+    #endregion
+    
+    #region Static Generate
+
+    public static BitFlag Get(uint power) => new((uint)MathF.Pow(2, power));
+    public static BitFlag Empty => new (0);
+
+    #endregion
+    
+    #region Static Int
+
+    public static int EmptyIntFlag() => 0;
+    public static int GetFlagInt(int power) => (int)MathF.Pow(2, power);
+    public static bool HasIntFlag(int flag, int value)
+    {
+        if (flag == 0) return false;
+        
+        return (flag & value) != 0;
+    }
+    public static int SetIntFlag(int flag, int value)
+    {
+        return flag ^= value;
+    }
+    public static int AddIntFlag(int flag, int value)
+    {
+        return flag |= value;
+    }
+    public static int RemoveIntFlag(int flag, int value)
+    {
+        return flag &= ~value;
+    }
+    public static int AddIntFlag(int flag, params int[] values)
+    {
+        foreach (var v in values)
+        {
+            flag |= v;
+        }
+
+        return flag;
+    }
+    public static int RemoveIntFlag(int flag, params int[] values)
+    {
+        foreach (var v in values)
+        {
+            flag &= ~v;
+        }
+
+        return flag;
+    }
+    #endregion
+    
+    #region Static Uint
+
+    public static uint EmptyUintFlag => 0;
+    public static uint GetFlagUint(uint power) => (uint)MathF.Pow(2, power);
+    public static bool HasUintFlag(uint flag, uint value)
+    {
+        if (flag == 0) return false;
+        
+        return (flag & value) != 0;
+    }
+    public static uint SetUintFlag(uint flag, uint value)
+    {
+        return flag ^= value;
+    }
+    public static uint AddUintFlag(uint flag, uint value)
+    {
+        return flag |= value;
+    }
+    public static uint RemoveUintFlag(uint flag, uint value)
+    {
+        return flag &= ~value;
+    }
+    public static uint AddUintFlag(uint flag, params uint[] values)
+    {
+        foreach (var v in values)
+        {
+            flag |= v;
+        }
+
+        return flag;
+    }
+    public static uint RemoveUintFlag(uint flag, params uint[] values)
+    {
+        foreach (var v in values)
+        {
+            flag &= ~v;
+        }
+
+        return flag;
+    }
+    #endregion
     
     #region Operators
     public static BitFlag operator ~(BitFlag value) =>new( ~value.Flag);
@@ -69,94 +202,95 @@ public readonly struct BitFlag : IBitwiseOperators<BitFlag, BitFlag, BitFlag>, I
 }
 
 
-public static class ShapeFlag
-{
-    #region Int
-
-    public static int GetFlagInt(int power) => (int)MathF.Pow(2, power);
-    public static int ClearIntFlag()
-    {
-        return 0;
-    }
-    public static bool HasIntFlag(this int flag, int value)
-    {
-        if (flag == 0) return true;//??
-        
-        return (flag & value) != 0;
-    }
-    public static int SetIntFlag(this int flag, int value)
-    {
-        return flag ^= value;
-    }
-    public static int AddIntFlag(this int flag, int value)
-    {
-        return flag |= value;
-    }
-    public static int RemoveIntFlag(this int flag, int value)
-    {
-        return flag &= ~value;
-    }
-    public static int AddIntFlag(this int flag, params int[] values)
-    {
-        foreach (var v in values)
-        {
-            flag |= v;
-        }
-
-        return flag;
-    }
-    public static int RemoveIntFlag(this int flag, params int[] values)
-    {
-        foreach (var v in values)
-        {
-            flag &= ~v;
-        }
-
-        return flag;
-    }
-    #endregion
-    
-    #region Uint
-    public static uint GetFlagUint(uint power) => (uint)MathF.Pow(2, power);
-    public static uint ClearUintFlag()
-    {
-        return 0;
-    }
-    public static bool HasUintFlag(this uint flag, uint value)
-    {
-        if (flag == 0) return true;//??
-        
-        return (flag & value) != 0;
-    }
-    public static uint SetUintFlag(this uint flag, uint value)
-    {
-        return flag ^= value;
-    }
-    public static uint AddUintFlag(this uint flag, uint value)
-    {
-        return flag |= value;
-    }
-    public static uint RemoveUintFlag(this uint flag, uint value)
-    {
-        return flag &= ~value;
-    }
-    public static uint AddUintFlag(this uint flag, params uint[] values)
-    {
-        foreach (var v in values)
-        {
-            flag |= v;
-        }
-
-        return flag;
-    }
-    public static uint RemoveUintFlag(this uint flag, params uint[] values)
-    {
-        foreach (var v in values)
-        {
-            flag &= ~v;
-        }
-
-        return flag;
-    }
-    #endregion
-}
+// public static class ShapeFlag
+// {
+//     #region Int
+//
+//     public static int GetFlagInt(int power) => (int)MathF.Pow(2, power);
+//     public static int ClearIntFlag()
+//     {
+//         return 0;
+//     }
+//     public static bool HasIntFlag(this int flag, int value)
+//     {
+//         if (flag == 0) return false;
+//         
+//         return (flag & value) != 0;
+//     }
+//     public static int SetIntFlag(this int flag, int value)
+//     {
+//         return flag ^= value;
+//     }
+//     public static int AddIntFlag(this int flag, int value)
+//     {
+//         return flag |= value;
+//     }
+//     public static int RemoveIntFlag(this int flag, int value)
+//     {
+//         return flag &= ~value;
+//     }
+//     public static int AddIntFlag(this int flag, params int[] values)
+//     {
+//         foreach (var v in values)
+//         {
+//             flag |= v;
+//         }
+//
+//         return flag;
+//     }
+//     public static int RemoveIntFlag(this int flag, params int[] values)
+//     {
+//         foreach (var v in values)
+//         {
+//             flag &= ~v;
+//         }
+//
+//         return flag;
+//     }
+//     #endregion
+//     
+//     #region Uint
+//     public static uint GetFlagUint(uint power) => (uint)MathF.Pow(2, power);
+//     public static uint ClearUintFlag()
+//     {
+//         return 0;
+//     }
+//     public static bool HasUintFlag(this uint flag, uint value)
+//     {
+//         if (flag == 0) return false;
+//         
+//         return (flag & value) != 0;
+//     }
+//     public static uint SetUintFlag(this uint flag, uint value)
+//     {
+//         return flag ^= value;
+//     }
+//     public static uint AddUintFlag(this uint flag, uint value)
+//     {
+//         return flag |= value;
+//     }
+//     public static uint RemoveUintFlag(this uint flag, uint value)
+//     {
+//         return flag &= ~value;
+//     }
+//     public static uint AddUintFlag(this uint flag, params uint[] values)
+//     {
+//         foreach (var v in values)
+//         {
+//             flag |= v;
+//         }
+//
+//         return flag;
+//     }
+//     public static uint RemoveUintFlag(this uint flag, params uint[] values)
+//     {
+//         foreach (var v in values)
+//         {
+//             flag &= ~v;
+//         }
+//
+//         return flag;
+//     }
+//     #endregion
+//     
+// }
