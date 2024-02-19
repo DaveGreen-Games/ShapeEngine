25a365252631cad613eb56af9535b128ba7baf9b
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-02-19 11:08:25 +0100

    quad collider improvements.

--- ShapeEngine/Core/Collision/QuadCollider.cs
+++ ShapeEngine/Core/Collision/QuadCollider.cs
@@ -7,30 +7,22 @@ namespace ShapeEngine.Core.Collision;
 
 public class QuadCollider : Collider
 {
-    public Vector2 A { get; set; }
-    public Vector2 B { get; set; }
-    public Vector2 C { get; set; }
-    public Vector2 D { get; set; }
-    // public float RotationDeg { get; set; }
-        
-    public QuadCollider(Vector2 a, Vector2 b, Vector2 c, Vector2 d, Vector2 offset) : base(offset)
+    public Vector2 Size { get; set; }
+    public Vector2 Alignement { get; set; }
+    
+    public QuadCollider(Vector2 size, Vector2 alignement, Vector2 offset) : base(offset)
     {
-        this.A = a;
-        this.B = b;
-        this.C = c;
-        this.D = d;
+        this.Size = size;
+        this.Alignement = alignement;
     }
-
+    public QuadCollider(Vector2 size, Vector2 alignement, Transform2D offset) : base(offset)
+    {
+        this.Size = size;
+        this.Alignement = alignement;
+    }
     public override bool ContainsPoint(Vector2 p) => GetQuadShape().ContainsPoint(p);
     public override Rect GetBoundingBox() => GetQuadShape().GetBoundingBox();
     public override CollisionPoint GetClosestCollisionPoint(Vector2 p) => GetQuadShape().GetClosestCollisionPoint(p);
     public override ShapeType GetShapeType() => ShapeType.Quad;
-    public override Quad GetQuadShape()
-    {
-        var finalA = CurTransform.Position + (A * CurTransform.Scale).Rotate(CurTransform.RotationRad);
-        var finalB = CurTransform.Position + (B * CurTransform.Scale).Rotate(CurTransform.RotationRad);
-        var finalC = CurTransform.Position + (C * CurTransform.Scale).Rotate(CurTransform.RotationRad);
-        var finalD = CurTransform.Position + (D * CurTransform.Scale).Rotate(CurTransform.RotationRad);
-        return new Quad(finalA, finalB, finalC, finalD);
-    }
+    public override Quad GetQuadShape() => new Quad(CurTransform.Position, Size * CurTransform.Scale,CurTransform.RotationRad, Alignement);
 }
