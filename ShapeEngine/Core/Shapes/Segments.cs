7e308ddef1a5430c72db20f964f90ecd670f0fa6
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-03-27 20:51:10 +0100

    segments & triangulation transform functions implemented.

--- ShapeEngine/Core/Shapes/Segments.cs
+++ ShapeEngine/Core/Shapes/Segments.cs
@@ -10,6 +10,7 @@ public class Segments : ShapeList<Segment>
 {
     #region Constructors
     public Segments() { }
+    public Segments(int capacity) : base(capacity) { }
     //public Segments(IShape shape) { AddRange(shape.GetEdges()); }
     public Segments(IEnumerable<Segment> edges) { AddRange(edges); }
     #endregion
@@ -197,6 +198,195 @@ public class Segments : ShapeList<Segment>
     }
     #endregion
 
+    #region Transform
+
+    
+    public void ChangeRotation(float rad, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ChangeRotation(rad, originF);
+        }
+    }
+    public void SetRotation(float rad, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].SetRotation(rad, originF);
+        }
+    }
+
+    public void ScaleLength(float scale, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ScaleLength(scale, originF);
+        }
+    }
+    public void ScaleLength(Size scale, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ScaleLength(scale, originF);
+        }
+    }
+    
+    public void ChangeLength(float amount, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ChangeLength(amount, originF);
+        }
+    }
+    public void SetSize(float length, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].SetLength(length, originF);
+        }
+    }
+    
+    public void ChangePosition(Vector2 offset)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ChangePosition(offset);
+        }
+    }
+    public void SetPosition(Vector2 position, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].SetPosition(position, originF);
+        }
+    }
+
+    public void ApplyTransform(Transform2D transform, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].ApplyTransform(transform, originF);
+        }
+    }
+    public void SetTransform(Transform2D transform, float originF = 0.5f)
+    {
+        for (var i = 0; i < Count; i++)
+        {
+            this[i] = this[i].SetTransform(transform, originF);
+        }
+    }
+
+    
+    
+    public Segments ChangeRotationCopy(float rad, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ChangeRotation(rad, originF));
+        }
+
+        return newSegments;
+    }
+    
+    public Segments SetRotationCopy(float rad, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].SetRotation(rad, originF));
+        }
+
+        return newSegments;
+    }
+
+    public Segments ScaleLengthCopy(float scale, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ScaleLength(scale, originF));
+        }
+
+        return newSegments;
+    }
+    public Segments ScaleLengthCopy(Size scale, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ScaleLength(scale, originF));
+        }
+
+        return newSegments;
+    }
+    
+    public Segments ChangeLengthCopy(float amount, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ChangeLength(amount, originF));
+        }
+
+        return newSegments;
+    }
+    
+    public Segments SetLengthCopy(float size, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].SetLength(size, originF));
+        }
+
+        return newSegments;
+    }
+    
+    public Segments ChangePositionCopy(Vector2 offset)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ChangePosition(offset));
+        }
+
+        return newSegments;
+    }
+    public Segments SetPositionCopy(Vector2 position, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].SetPosition(position, originF));
+        }
+
+        return newSegments;
+    }
+
+    public Segments ApplyTransformCopy(Transform2D transform, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].ApplyTransform(transform, originF));
+        }
+
+        return newSegments;
+    }
+    public Segments SetTransformCopy(Transform2D transform, float originF = 0.5f)
+    {
+        var newSegments = new Segments(Count);
+        for (var i = 0; i < Count; i++)
+        {
+            newSegments.Add(this[i].SetTransform(transform, originF));
+        }
+
+        return newSegments;
+    }
+
+    #endregion
+    
     #region Overlap
     public bool OverlapShape(Segments b)
     {
