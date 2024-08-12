48c31f5593eeeab7d6da8151c25ab67705ac4291
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-08-12 09:17:47 +0200

    ScreenInfo.cs relative mouse position added.

--- ShapeEngine/Core/Structs/ScreenInfo.cs
+++ ShapeEngine/Core/Structs/ScreenInfo.cs
@@ -7,6 +7,13 @@ public readonly struct ScreenInfo
     public readonly Rect Area;
     public readonly Vector2 MousePos;
 
+    /// <summary>
+    /// Returns the mouse position in a range between 0 and 1.
+    /// 0,0 is the topleft corner of the area
+    /// 1, 1 is the bottom right corner of the area
+    /// </summary>
+    public Vector2 RelativeMousePosition => MousePos / Area.Size.ToVector2();
+
     public ScreenInfo(Rect area, Vector2 mousePos)
     {
         this.Area = area;
