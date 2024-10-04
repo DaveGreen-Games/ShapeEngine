4992f6ea9ecc7d6a1df4b9f5785635d98087c800
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-10-04 08:46:21 +0200

    ValueRange and ValueRangeInt improvements/ clean up.

--- ShapeEngine/Core/ValueRange.cs
+++ ShapeEngine/Core/ValueRange.cs
@@ -9,8 +9,9 @@ public readonly struct ValueRange
     public readonly float Min;
     public readonly float Max;
 
-    public ValueRange() { Min = 0.0f; Max = 1.0f; }
+    #region Constructors
 
+    public ValueRange() { Min = 0.0f; Max = 1.0f; }
     public ValueRange(Vector2 range)
     {
         float min = range.X;
@@ -39,6 +40,19 @@ public readonly struct ValueRange
             this.Max = max;
         }
     }
+    public ValueRange(int min, int max)
+    {
+        if (min > max)
+        {
+            this.Max = min;
+            this.Min = max;
+        }
+        else
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+    }
     public ValueRange(float max)
     {
         if (max < 0.0f)
@@ -52,8 +66,10 @@ public readonly struct ValueRange
             this.Max = max;
         }
     }
-    
+    #endregion
 
+    #region Public Functions
+    public ValueRangeInt ToValueRangeInt() => new ValueRangeInt(Min, Max);
     public Vector2 ToVector2() => new(Min, Max);
     public float Rand() { return Rng.Instance.RandF(Min, Max); }
     public float Lerp(float f) { return ShapeMath.LerpFloat(Min, Max, f); }
@@ -71,4 +87,114 @@ public readonly struct ValueRange
         var bRange = new ValueRange(bMin, bMax);
         return aRange.OverlapValueRange(bRange);
     }
+    
+    public ValueRange SetMin(float min) => new(min, Max);
+    public ValueRange SetMax(float max) => new(Min, max);
+    public ValueRange Set(float min, float max) => new(min, max);
+    
+    #endregion
+    
+    #region Operators
+    public static ValueRange operator +(ValueRange left, ValueRange right)
+    {
+        return new(
+            left.Min + right.Min, 
+            left.Max + right.Max);
+    }
+    public static ValueRange operator -(ValueRange left, ValueRange right)
+    {
+        return new(
+            left.Min - right.Min, 
+            left.Max - right.Max);
+    }
+    public static ValueRange operator *(ValueRange left, ValueRange right)
+    {
+        return new(
+            left.Min * right.Min, 
+            left.Max * right.Max);
+    }
+    public static ValueRange operator /(ValueRange left, ValueRange right)
+    {
+        return new(
+            right.Min == 0 ? 0 : left.Min / right.Min, 
+            right.Max == 0 ? 0 :left.Max / right.Max);
+    }
+    
+    public static ValueRange operator +(ValueRange left, ValueRangeInt right)
+    {
+        return new(
+            left.Min + right.Min, 
+            left.Max + right.Max);
+    }
+    public static ValueRange operator -(ValueRange left, ValueRangeInt right)
+    {
+        return new(
+            left.Min - right.Min, 
+            left.Max - right.Max);
+    }
+    public static ValueRange operator *(ValueRange left, ValueRangeInt right)
+    {
+        return new(
+            left.Min * right.Min, 
+            left.Max * right.Max);
+    }
+    public static ValueRange operator /(ValueRange left, ValueRangeInt right)
+    {
+        return new(
+            right.Min == 0 ? 0 : left.Min / right.Min, 
+            right.Max == 0 ? 0 :left.Max / right.Max);
+    }
+    
+    public static ValueRange operator +(ValueRange left, float right)
+    {
+        return new(
+            left.Min + right, 
+            left.Max + right);
+    }
+    public static ValueRange operator -(ValueRange left, float right)
+    {
+        return new(
+            left.Min - right, 
+            left.Max - right);
+    }
+    public static ValueRange operator *(ValueRange left, float right)
+    {
+        return new(
+            left.Min * right, 
+            left.Max * right);
+    }
+    public static ValueRange operator /(ValueRange left, float right)
+    {
+        if (right == 0) return new(0, 0);
+        return new(
+            left.Min / right, 
+            left.Max / right);
+    }
+    
+    public static ValueRange operator +(ValueRange left, int right)
+    {
+        return new(
+            left.Min + right, 
+            left.Max + right);
+    }
+    public static ValueRange operator -(ValueRange left, int right)
+    {
+        return new(
+            left.Min - right, 
+            left.Max - right);
+    }
+    public static ValueRange operator *(ValueRange left, int right)
+    {
+        return new(
+            left.Min * right, 
+            left.Max * right);
+    }
+    public static ValueRange operator /(ValueRange left, int right)
+    {
+        if (right == 0) return new(0, 0);
+        return new(
+            left.Min / right, 
+            left.Max / right);
+    }
+    #endregion
 }
--- ShapeEngine/Core/ValueRangeInt.cs
+++ ShapeEngine/Core/ValueRangeInt.cs
@@ -9,8 +9,9 @@ public readonly struct ValueRangeInt
     public readonly int Min;
     public readonly int Max;
 
+    #region Constructors
+    
     public ValueRangeInt() { Min = 0; Max = 1; }
-
     public ValueRangeInt(Vector2 range)
     {
         var min = (int)range.X;
@@ -39,6 +40,19 @@ public readonly struct ValueRangeInt
             Max = max;
         }
     }
+    public ValueRangeInt(float min, float max)
+    {
+        if (min > max)
+        {
+            Max = (int)min;
+            Min = (int)max;
+        }
+        else
+        {
+            Min = (int)min;
+            Max = (int)max;
+        }
+    }
     public ValueRangeInt(int max)
     {
         if (max < 0.0f)
@@ -53,7 +67,11 @@ public readonly struct ValueRangeInt
         }
     }
     
-
+    #endregion
+    
+    #region Public Functions
+    
+    public ValueRange ToValueRange() => new ValueRange(Min, Max);
     public Vector2 ToVector2() => new(Min, Max);
     public int Rand() { return Rng.Instance.RandI(Min, Max); }
     public int Lerp(float f) { return ShapeMath.LerpInt(Min, Max, f); }
@@ -65,4 +83,114 @@ public readonly struct ValueRangeInt
     public int Clamp(int value) => ShapeMath.Clamp(value, Min, Max);
     
     public bool OverlapValueRange(ValueRangeInt other) => other.Min <= Max && Min <= other.Max;
+    
+    public ValueRangeInt SetMin(int min) => new(min, Max);
+    public ValueRangeInt SetMax(int max) => new(Min, max);
+    public ValueRangeInt Set(int min, int max) => new(min, max);
+    #endregion
+    
+    #region Operators
+    
+    public static ValueRangeInt operator +(ValueRangeInt left, ValueRangeInt right)
+    {
+        return new(
+            left.Min + right.Min, 
+            left.Max + right.Max);
+    }
+    public static ValueRangeInt operator -(ValueRangeInt left, ValueRangeInt right)
+    {
+        return new(
+            left.Min - right.Min, 
+            left.Max - right.Max);
+    }
+    public static ValueRangeInt operator *(ValueRangeInt left, ValueRangeInt right)
+    {
+        return new(
+            left.Min * right.Min, 
+            left.Max * right.Max);
+    }
+    public static ValueRangeInt operator /(ValueRangeInt left, ValueRangeInt right)
+    {
+        return new(
+            right.Min == 0 ? 0 : left.Min / right.Min, 
+            right.Max == 0 ? 0 :left.Max / right.Max);
+    }
+    
+    public static ValueRangeInt operator +(ValueRangeInt left, ValueRange right)
+    {
+        return new(
+            left.Min + right.Min, 
+            left.Max + right.Max);
+    }
+    public static ValueRangeInt operator -(ValueRangeInt left, ValueRange right)
+    {
+        return new(
+            left.Min - right.Min, 
+            left.Max - right.Max);
+    }
+    public static ValueRangeInt operator *(ValueRangeInt left, ValueRange right)
+    {
+        return new(
+            left.Min * right.Min, 
+            left.Max * right.Max);
+    }
+    public static ValueRangeInt operator /(ValueRangeInt left, ValueRange right)
+    {
+        return new(
+            right.Min == 0 ? 0 : left.Min / right.Min, 
+            right.Max == 0 ? 0 :left.Max / right.Max);
+    }
+    
+    public static ValueRangeInt operator +(ValueRangeInt left, float right)
+    {
+        return new(
+            left.Min + (int)right, 
+            left.Max + (int)right);
+    }
+    public static ValueRangeInt operator -(ValueRangeInt left, float right)
+    {
+        return new(
+            left.Min - (int)right, 
+            left.Max - (int)right);
+    }
+    public static ValueRangeInt operator *(ValueRangeInt left, float right)
+    {
+        return new(
+            (int)(left.Min * right), 
+            (int)(left.Max * right));
+    }
+    public static ValueRangeInt operator /(ValueRangeInt left, float right)
+    {
+        if (right == 0) return new(0, 0);
+        return new(
+            (int)(left.Min / right), 
+            (int)(left.Max / right));
+    }
+    
+    public static ValueRangeInt operator +(ValueRangeInt left, int right)
+    {
+        return new(
+            left.Min + right, 
+            left.Max + right);
+    }
+    public static ValueRangeInt operator -(ValueRangeInt left, int right)
+    {
+        return new(
+            left.Min - right, 
+            left.Max - right);
+    }
+    public static ValueRangeInt operator *(ValueRangeInt left, int right)
+    {
+        return new(
+            left.Min * right, 
+            left.Max * right);
+    }
+    public static ValueRangeInt operator /(ValueRangeInt left, int right)
+    {
+        if (right == 0) return new(0, 0);
+        return new(
+            left.Min / right, 
+            left.Max / right);
+    }
+    #endregion
 }
