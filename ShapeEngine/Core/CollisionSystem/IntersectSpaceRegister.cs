a0ce5e34d67ad216af474d09f56d5f74a57b8ba9
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-11-20 21:28:56 +0100

    IntersectSpace Entry/Register/Result classes improved, cleaned up, filter & sorting functions added.

--- ShapeEngine/Core/CollisionSystem/IntersectSpaceRegister.cs
+++ ShapeEngine/Core/CollisionSystem/IntersectSpaceRegister.cs
@@ -1,16 +1,27 @@
 using System.Numerics;
+using ShapeEngine.Core.Structs;
 
 namespace ShapeEngine.Core.CollisionSystem;
 
 public class IntersectSpaceRegister : List<IntersectSpaceEntry>
 {
+    #region Members
     public readonly CollisionObject Object;
+    public IntersectSpaceEntry? First => Count <= 0 ? null : this[0];
+    public IntersectSpaceEntry? Last => Count <= 0 ? null : this[Count - 1];
 
+    #endregion
+    
+    #region Constructors
     public IntersectSpaceRegister(CollisionObject colObject, int capacity) : base(capacity)
     {
         Object = colObject;
     }
 
+    #endregion
+    
+    #region Public Functions
+    
     public bool AddEntry(IntersectSpaceEntry entry)
     {
         if (entry.Count <= 0) return false;
@@ -19,8 +30,17 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
         return true;
     }
 
-    public void SortClosestFirst(Vector2 referencePoint)
+    #endregion
+    
+    #region Sorting
+    public bool SortClosestFirst(Vector2 referencePoint)
     {
+        if (Count <= 0) return false;
+        if (Count == 1)
+        {
+            this[0].SortClosestFirst(referencePoint);
+            return true;
+        }
         foreach (var entry in this)
         {
             entry.SortClosestFirst(referencePoint);
@@ -29,6 +49,9 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
         (
             comparison: (a, b) =>
             {
+                if (a.Count <= 0) return 1;
+                if (b.Count <= 0) return -1;
+                
                 float la = (referencePoint - a.First.Point).LengthSquared();
                 float lb = (referencePoint - b.First.Point).LengthSquared();
 
@@ -37,9 +60,16 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
                 return -1;
             }
         );
+        return true;
     }
-    public void SortFurthestFirst(Vector2 referencePoint)
+    public bool SortFurthestFirst(Vector2 referencePoint)
     {
+        if (Count <= 0) return false;
+        if (Count == 1)
+        {
+            this[0].SortFurthestFirst(referencePoint);
+            return true;
+        }
         foreach (var entry in this)
         {
             entry.SortFurthestFirst(referencePoint);
@@ -48,6 +78,9 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
         (
             comparison: (a, b) =>
             {
+                if (a.Count <= 0) return -1;
+                if (b.Count <= 0) return 1;
+                
                 float la = (referencePoint - a.First.Point).LengthSquared();
                 float lb = (referencePoint - b.First.Point).LengthSquared();
 
@@ -56,21 +89,74 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
                 return -1;
             }
         );
+        return true;
     }
 
-    public IntersectSpaceEntry? GetClosestEntry(Vector2 referencePoint, out float closestDistanceSquared)
+    #endregion
+    
+    #region Closest/Furthest Collider
+    public IntersectSpaceEntry? GetClosestEntryCollider(Vector2 referencePoint, out float closestDistanceSquared)
     {
         closestDistanceSquared = -1;
         if(Count <= 0) return null;
         if(Count == 1) return this[0];
         
         var closestEntry = this[0];
-        closestDistanceSquared = (referencePoint - closestEntry.First.Point).LengthSquared();
+        closestDistanceSquared = (referencePoint - closestEntry.Collider.CurTransform.Position).LengthSquared();
         for (int i = 1; i < Count; i++)
         {
             var entry = this[i];
-            var closestPoint = entry.GetClosestCollisionPoint(referencePoint, out var distanceSquared);
-            // var distanceSquared = (referencePoint - closestPoint.Point).LengthSquared();
+            var distanceSquared = (referencePoint - entry.Collider.CurTransform.Position).LengthSquared();
+            if (distanceSquared < closestDistanceSquared)
+            {
+                closestDistanceSquared = distanceSquared;
+                closestEntry = entry;
+            }
+        }
+        return closestEntry;
+    }
+    public IntersectSpaceEntry? GetFurthestEntryCollider(Vector2 referencePoint, out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        if(Count == 1) return this[0];
+        
+        var furthestEntry = this[0];
+        furthestDistanceSquared = (referencePoint - furthestEntry.Collider.CurTransform.Position).LengthSquared();
+        for (int i = 1; i < Count; i++)
+        {
+            var entry = this[i];
+            var distanceSquared = (referencePoint - entry.Collider.CurTransform.Position).LengthSquared();
+            if (distanceSquared > furthestDistanceSquared)
+            {
+                furthestDistanceSquared = distanceSquared;
+                furthestEntry = entry;
+            }
+        }
+        return furthestEntry;
+    }
+    
+    #endregion
+    
+    #region Closest/Furthest Entry
+    
+    public IntersectSpaceEntry? GetClosestEntry(Vector2 referencePoint, out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var entry = this[0];
+            entry.GetClosestCollisionPoint(referencePoint, out closestDistanceSquared);
+            return entry;
+        }
+        
+        var closestEntry = this[0];
+        closestEntry.GetClosestCollisionPoint(referencePoint, out closestDistanceSquared);
+        for (int i = 1; i < Count; i++)
+        {
+            var entry = this[i];
+            entry.GetClosestCollisionPoint(referencePoint, out var distanceSquared);
             if (distanceSquared < closestDistanceSquared)
             {
                 closestDistanceSquared = distanceSquared;
@@ -83,16 +169,20 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
     {
         furthestDistanceSquared = -1;
         if(Count <= 0) return null;
-        if(Count == 1) return this[0];
+        if (Count == 1)
+        {
+            var entry = this[0];
+            entry.GetFurthestCollisionPoint(referencePoint, out furthestDistanceSquared);
+            return entry;
+        }
         
         var furthestEntry = this[0];
-        furthestDistanceSquared = (referencePoint - furthestEntry.First.Point).LengthSquared();
+        furthestEntry.GetFurthestCollisionPoint(referencePoint, out furthestDistanceSquared);
         for (int i = 1; i < Count; i++)
         {
             var entry = this[i];
-            var furthestPoint = entry.GetFurthestCollisionPoint(referencePoint, out var distanceSquared);
-            // var distanceSquared = (referencePoint - furthestPoint.Point).LengthSquared();
-            if (distanceSquared > furthestDistanceSquared)
+            entry.GetFurthestCollisionPoint(referencePoint, out var distanceSquared);
+            if (distanceSquared < furthestDistanceSquared)
             {
                 furthestDistanceSquared = distanceSquared;
                 furthestEntry = entry;
@@ -101,9 +191,52 @@ public class IntersectSpaceRegister : List<IntersectSpaceEntry>
         return furthestEntry;
     }
 
-    //TODO: Add filter / query methods here to get specific IntersectSpaceEntries
-    //--- Find Closest/Furhtest Entry. (based on Collider transform position) 
-    //--- Find Closest/Furhtest Entry. (based on CollisionPoint) 
-    //--- Find CLosest/Furthest CollisionPoint 
-    //--- Validate all entries and get closest collision point/entry
+    #endregion
+    
+    #region Closest/Furthest Collision Point
+    public CollisionPoint GetClosestCollisionPoint(Vector2 referencePoint, out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if(Count <= 0) return new();
+        if(Count == 1) return this[0].GetClosestCollisionPoint(referencePoint, out closestDistanceSquared);
+        
+        var closestPoint = this[0].GetClosestCollisionPoint(referencePoint, out closestDistanceSquared);
+        for (int i = 1; i < Count; i++)
+        {
+            var point = this[i].GetClosestCollisionPoint(referencePoint, out var distanceSquared);
+            
+            if (distanceSquared < closestDistanceSquared)
+            {
+                closestDistanceSquared = distanceSquared;
+                closestPoint = point;
+            }
+        }
+        return closestPoint;
+    }
+    public CollisionPoint GetFurthestCollisionPoint(Vector2 referencePoint, out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if(Count <= 0) return new();
+        if(Count == 1) return this[0].GetFurthestCollisionPoint(referencePoint, out furthestDistanceSquared);
+        
+        var furthestPoint = this[0].GetFurthestCollisionPoint(referencePoint, out furthestDistanceSquared);
+        for (int i = 1; i < Count; i++)
+        {
+            var point = this[i].GetFurthestCollisionPoint(referencePoint, out var distanceSquared);
+            
+            if (distanceSquared > furthestDistanceSquared)
+            {
+                furthestDistanceSquared = distanceSquared;
+                furthestPoint = point;
+            }
+        }
+        return furthestPoint;
+    }
+
+    #endregion
+    
+    #region Validation
+    //TODO: Validate all entries and get closest collision point/entry
+    
+    #endregion
 }
--- ShapeEngine/Core/CollisionSystem/IntersectSpaceResult.cs
+++ ShapeEngine/Core/CollisionSystem/IntersectSpaceResult.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.Numerics;
 using ShapeEngine.Core.Structs;
 
@@ -5,45 +6,133 @@ namespace ShapeEngine.Core.CollisionSystem;
 
 public class IntersectSpaceResult : List<IntersectSpaceRegister>
 {
-    public readonly Vector2 Origin;
+    #region Members
     
+    public readonly Vector2 Origin;
+    public IntersectSpaceRegister? First => Count <= 0 ? null : this[0];
+    public IntersectSpaceRegister? Last => Count <= 0 ? null : this[Count - 1];
+    
+    #endregion
+    
+    #region Constructors
     public IntersectSpaceResult(Vector2 origin, int capacity) : base(capacity)
     {
         Origin = origin;
     }
 
+    #endregion
+    
+    #region Public Functions
+    
     public bool AddRegister(IntersectSpaceRegister reg)
     {
         if (reg.Count <= 0) return false;
         Add(reg);
         return true;
     }
+    
+    #endregion
+    
+    #region Sorting
+    public bool SortClosestFirst() => SortClosestFirst(Origin);
+    public bool SortFurthestFirst() => SortFurthestFirst(Origin);
 
-    public void SortClosestFirst()
+    public bool SortClosestFirst(Vector2 referencePoint)
     {
-        foreach (var reg in this)
+        if(Count <= 0) return false;
+        if (Count == 1)
         {
-            reg.SortClosestFirst(Origin);
-        }    
-    }
-    public void SortFurthestFirst()
-    {
-        foreach (var reg in this)
-        {
-            reg.SortFurthestFirst(Origin);
+            this[0].SortClosestFirst(referencePoint);
+            return true;
         }
+        foreach (var reg in this)
+        {
+            reg.SortClosestFirst(referencePoint);
+        }  
+        this.Sort
+        (
+            comparison: (a, b) =>
+            {
+                if (a.Count <= 0) return 1;
+                if (b.Count <= 0) return -1;
+                
+                var aEntry = a.First;
+                if(aEntry == null) return 1;
+                
+                var bEntry = b.First;
+                if(bEntry == null) return -1;
+                
+                float la = (referencePoint - aEntry.First.Point).LengthSquared();
+                float lb = (referencePoint - bEntry.First.Point).LengthSquared();
+
+                if (la > lb) return 1;
+                if (MathF.Abs(x: la - lb) < 0.01f) return 0;
+                return -1;
+            }
+        );
+        return true;
+    }
+    public bool SortFurthestFirst(Vector2 referencePoint)
+    {
+        if(Count <= 0) return false;
+        if (Count == 1)
+        {
+            this[0].SortFurthestFirst(referencePoint);
+            return true;
+        }
+        foreach (var reg in this)
+        {
+            reg.SortFurthestFirst(referencePoint);
+        }
+        this.Sort
+        (
+            comparison: (a, b) =>
+            {
+                if (a.Count <= 0) return -1;
+                if (b.Count <= 0) return 1;
+                
+                var aEntry = a.First;
+                if(aEntry == null) return -1;
+                
+                var bEntry = b.First;
+                if(bEntry == null) return 1;
+                
+                float la = (referencePoint - aEntry.First.Point).LengthSquared();
+                float lb = (referencePoint - bEntry.First.Point).LengthSquared();
+
+                if (la > lb) return 1;
+                if (MathF.Abs(x: la - lb) < 0.01f) return 0;
+                return -1;
+            }
+        );
+        return true;
+    }
+    #endregion
+    
+    #region Validation
+    //TODO: validate functions for Result, Register, and Entry
+    
+    #endregion
+    
+    #region Closest/Furthest Entry
+    
+    public IntersectSpaceEntry? GetClosestEntry(out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if (Count <= 0) return null;
+        return GetClosestEntry(Origin, out closestDistanceSquared);
     }
 
-    public IntersectSpaceEntry? GetClosestEntry()
+    public IntersectSpaceEntry? GetClosestEntry(Vector2 referencePoint, out float closestDistanceSquared)
     {
+        closestDistanceSquared = -1f;
         if (Count <= 0) return null;
-        return GetClosestEntry(Origin);
-    }
-    public IntersectSpaceEntry? GetClosestEntry(Vector2 referencePoint)
-    {
-        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            return reg.GetClosestEntry(referencePoint, out closestDistanceSquared);
+        }
         IntersectSpaceEntry? closestEntry = null;
-        var closestDistanceSquared = 0f;
         foreach (var reg in this)
         {
             var entry = reg.GetClosestEntry(referencePoint, out var distanceSquared);
@@ -61,25 +150,239 @@ public class IntersectSpaceResult : List<IntersectSpaceRegister>
         }
         return closestEntry;
     }
-    public CollisionPoint GetClosestCollisionPoint()
+    public IntersectSpaceEntry? GetFurthestEntry(out float furthestDistanceSquared)
     {
-        return GetClosestCollisionPoint(Origin);
+        furthestDistanceSquared = -1f;
+        if (Count <= 0) return null;
+        return GetFurthestEntry(Origin, out furthestDistanceSquared);
     }
-    public CollisionPoint GetClosestCollisionPoint(Vector2 position)
+    public IntersectSpaceEntry? GetFurthestEntry(Vector2 referencePoint, out float furthestDistanceSquared)
     {
+        furthestDistanceSquared = -1f;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            return reg.GetFurthestEntry(referencePoint, out furthestDistanceSquared);
+        }
+        IntersectSpaceEntry? furthestEntry = null;
         foreach (var reg in this)
         {
+            var entry = reg.GetFurthestEntry(referencePoint, out var distanceSquared);
+            if (furthestEntry == null)
+            {
+                furthestEntry = entry;
+                furthestDistanceSquared = distanceSquared;
+            }
+            else if (distanceSquared > furthestDistanceSquared)
+            {
+                furthestEntry = entry;
+                furthestDistanceSquared = distanceSquared;
+            }
             
         }
-
-        return new();
+        return furthestEntry;
     }
+
+    #endregion
     
+    #region Closest/Furthest Collider
     
-    //TODO: Add filter / query methods here to get specific IntersectSpaceResults
-    //--- Find Closest/Furthest Register (based on Object transform position)
-    //--- Find Closest/Furhtest Entry. (based on Collider transform position) 
-    //--- Find Closest/Furhtest Entry. (based on CollisionPoint) 
-    //--- Find CLosest/Furthest CollisionPoint 
-    //--- Validate all registers and get closest collision point/entry
+    public IntersectSpaceEntry? GetClosestEntryCollider(out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if (Count <= 0) return null;
+        return GetClosestEntryCollider(Origin, out closestDistanceSquared);
+    }
+    public IntersectSpaceEntry? GetClosestEntryCollider(Vector2 referencePoint, out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1f;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            return reg.GetClosestEntryCollider(referencePoint, out closestDistanceSquared);
+        }
+        IntersectSpaceEntry? closestEntry = null;
+        foreach (var reg in this)
+        {
+            var entry = reg.GetClosestEntryCollider(referencePoint, out var distanceSquared);
+            if (closestEntry == null)
+            {
+                closestEntry = entry;
+                closestDistanceSquared = distanceSquared;
+            }
+            else if (distanceSquared < closestDistanceSquared)
+            {
+                closestEntry = entry;
+                closestDistanceSquared = distanceSquared;
+            }
+            
+        }
+        return closestEntry;
+    }
+    public IntersectSpaceEntry? GetFurthestEntryCollider(out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1f;
+        if (Count <= 0) return null;
+        return GetFurthestEntryCollider(Origin, out furthestDistanceSquared);
+    }
+    public IntersectSpaceEntry? GetFurthestEntryCollider(Vector2 referencePoint, out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1f;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            return reg.GetFurthestEntryCollider(referencePoint, out furthestDistanceSquared);
+        }
+        IntersectSpaceEntry? furthestEntry = null;
+        foreach (var reg in this)
+        {
+            var entry = reg.GetFurthestEntryCollider(referencePoint, out var distanceSquared);
+            if (furthestEntry == null)
+            {
+                furthestEntry = entry;
+                furthestDistanceSquared = distanceSquared;
+            }
+            else if (distanceSquared > furthestDistanceSquared)
+            {
+                furthestEntry = entry;
+                furthestDistanceSquared = distanceSquared;
+            }
+            
+        }
+        return furthestEntry;
+    }
+
+    #endregion
+    
+    #region Closest/Furthest Register
+    
+    public IntersectSpaceRegister? GetClosestRegister(out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        return GetClosestRegister(Origin, out closestDistanceSquared);
+    }
+    public IntersectSpaceRegister? GetClosestRegister(Vector2 referencePoint, out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            closestDistanceSquared = (referencePoint - reg.Object.Transform.Position).LengthSquared();
+            return reg;
+        }
+        
+        var closestReg = this[0];
+        closestDistanceSquared = (referencePoint - closestReg.Object.Transform.Position).LengthSquared();
+        for (int i = 1; i < Count; i++)
+        {
+            var reg = this[i];
+            var distanceSquared = (referencePoint - reg.Object.Transform.Position).LengthSquared();
+            if (distanceSquared < closestDistanceSquared)
+            {
+                closestDistanceSquared = distanceSquared;
+                closestReg = reg;
+            }
+        }
+        return closestReg;
+    }
+    public IntersectSpaceRegister? GetFurthestRegister(out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        return GetFurthestRegister(Origin, out furthestDistanceSquared);
+    }
+    public IntersectSpaceRegister? GetFurthestRegister(Vector2 referencePoint, out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if(Count <= 0) return null;
+        if (Count == 1)
+        {
+            var reg = this[0];
+            furthestDistanceSquared = (referencePoint - reg.Object.Transform.Position).LengthSquared();
+            return this[0];
+        }
+        
+        var furthestReg = this[0];
+        furthestDistanceSquared = (referencePoint - furthestReg.Object.Transform.Position).LengthSquared();
+        for (int i = 1; i < Count; i++)
+        {
+            var reg = this[i];
+            var distanceSquared = (referencePoint - reg.Object.Transform.Position).LengthSquared();
+            if (distanceSquared > furthestDistanceSquared)
+            {
+                furthestDistanceSquared = distanceSquared;
+                furthestReg = reg;
+            }
+        }
+        return furthestReg;
+    }
+
+    #endregion
+    
+    #region Closest/Furthest Collision Points
+    
+    public CollisionPoint GetClosestCollisionPoint(out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if (Count <= 0) return new();
+        return GetClosestCollisionPoint(Origin, out closestDistanceSquared);
+    }
+    public CollisionPoint GetClosestCollisionPoint(Vector2 position, out float closestDistanceSquared)
+    {
+        closestDistanceSquared = -1;
+        if (Count <= 0) return new();
+        if (Count == 1)
+        {
+            var point = this[0].GetClosestCollisionPoint(position, out closestDistanceSquared);
+            return point;
+        }
+        var closestPoint = this[0].GetClosestCollisionPoint(position, out closestDistanceSquared);
+        for (int i = 1; i < Count; i++)
+        {
+            var point = this[i].GetClosestCollisionPoint(position, out var distanceSquared);
+            if (distanceSquared < closestDistanceSquared)
+            {
+                closestDistanceSquared = distanceSquared;
+                closestPoint = point;
+            }
+        }
+
+        return closestPoint;
+    }
+    public CollisionPoint GetFurthestCollisionPoint(out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if (Count <= 0) return new();
+        return GetFurthestCollisionPoint(Origin, out furthestDistanceSquared);
+    }
+    public CollisionPoint GetFurthestCollisionPoint(Vector2 position, out float furthestDistanceSquared)
+    {
+        furthestDistanceSquared = -1;
+        if (Count <= 0) return new();
+        if (Count == 1)
+        {
+            var point = this[0].GetFurthestCollisionPoint(position, out furthestDistanceSquared);
+            return point;
+        }
+        var furthestPoint = this[0].GetClosestCollisionPoint(position, out furthestDistanceSquared);
+        for (int i = 1; i < Count; i++)
+        {
+            var point = this[i].GetFurthestCollisionPoint(position, out var distanceSquared);
+            if (distanceSquared < furthestDistanceSquared)
+            {
+                furthestDistanceSquared = distanceSquared;
+                furthestPoint = point;
+            }
+        }
+
+        return furthestPoint;
+    }
+   
+    #endregion
+    
 }
