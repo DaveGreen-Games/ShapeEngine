e90917fc73e737330b0337e1ff027dc1ae808045
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-11-24 14:32:30 +0100

    CollisionPoint improvements. - Operators added - SetNormal/Point functions added - RotateNormal functions added

--- ShapeEngine/Core/Structs/CollisionPoint.cs
+++ ShapeEngine/Core/Structs/CollisionPoint.cs
@@ -5,10 +5,15 @@ namespace ShapeEngine.Core.Structs;
 
 public readonly struct CollisionPoint : IEquatable<CollisionPoint>
 {
+    #region Members
+    
     public bool Valid => Normal.X != 0f || Normal.Y != 0f;
     public readonly Vector2 Point;
     public readonly Vector2 Normal;
 
+    #endregion
+    
+    #region Constructors
     public CollisionPoint() 
     { 
         Point = new(); 
@@ -21,8 +26,11 @@ public readonly struct CollisionPoint : IEquatable<CollisionPoint>
         Normal = n;
     }
     
+    #endregion
+    
+    #region Public Functions
     public CollisionPoint Average(CollisionPoint other) => new((Point + other.Point) / 2, (Normal + other.Normal).Normalize());
-
+    
     public static CollisionPoint Average(CollisionPoint a, CollisionPoint b) => new((a.Point + b.Point) / 2, (a.Normal + b.Normal).Normalize());
 
     public static CollisionPoint Average(params CollisionPoint[] points)
@@ -62,4 +70,110 @@ public readonly struct CollisionPoint : IEquatable<CollisionPoint>
     public bool IsNormalFacing(Vector2 referenceDir) => Normal.IsFacingTheSameDirection(referenceDir);
     public bool IsNormalFacingPoint(Vector2 referencePoint) => IsNormalFacing(referencePoint - Point);
     
+    #endregion
+    
+    #region Math
+
+    public CollisionPoint RotateNormal(float angleRad) => !Valid ? this : new(Point, Normal.Rotate(angleRad));
+
+    public CollisionPoint RotateNormalDeg(float angleDeg) => !Valid ? this : new(Point, Normal.Rotate(angleDeg * ShapeMath.DEGTORAD));
+
+    public CollisionPoint SetPoint(Vector2 newPoint) => new(newPoint, Normal);
+
+    public CollisionPoint SetNormal(Vector2 newNormal) => new(Point, newNormal.Normalize());
+
+    #endregion
+    
+    #region Operators
+    
+    /// <summary>
+    /// Add point a to point b, and add normal a to normal b and normailze it.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator +(CollisionPoint a, CollisionPoint b)
+    {
+        return new(a.Point + b.Point, (a.Normal + b.Normal).Normalize());
+    }
+
+    /// <summary>
+    /// Subtract point b from point a, and subtract normal b from normal a and normailze it.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator -(CollisionPoint a, CollisionPoint b)
+    {
+        return new(a.Point - b.Point, (a.Normal - b.Normal).Normalize());
+    }
+
+    /// <summary>
+    /// Multiply point a by scalar, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator *(CollisionPoint a, float scalar)
+    {
+        return new(a.Point * scalar, a.Normal);
+    }
+
+    /// <summary>
+    /// Divide point a by scalar, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="scalar"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator /(CollisionPoint a, float scalar)
+    {
+        return new(a.Point / scalar, a.Normal);
+    }
+    
+    /// <summary>
+    /// Add vector b to point a, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator +(CollisionPoint a, Vector2  b)
+    {
+        return new(a.Point + b, a.Normal);
+    }
+
+    /// <summary>
+    /// Subtract vector b from point a, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator -(CollisionPoint a, Vector2  b)
+    {
+        return new(a.Point - b, a.Normal);
+    }
+
+    /// <summary>
+    /// Multiply point a by vector b, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator *(CollisionPoint a, Vector2 b)
+    {
+        return new(a.Point * b, a.Normal);
+    }
+
+    /// <summary>
+    /// Divide point a by vector b, and keep normal unchanged.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static CollisionPoint operator /(CollisionPoint a, Vector2 b)
+    {
+        return new(a.Point / b, a.Normal);
+    }
+    
+    
+    #endregion
 }
