9f43ff5ae0124c038092d5fdf4b0b181bd1fa785
Dave Green <34277803+SoloByte@users.noreply.github.com>
2024-12-16 20:48:40 +0100

    CollisionPoint  - Average() functions renamed to Combine().  - IsClosest() function added  - IsFurthest() function added  - IsPointingTowards() function added  - IsPointingAway() function added.

--- ShapeEngine/Core/Structs/CollisionPoint.cs
+++ ShapeEngine/Core/Structs/CollisionPoint.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using ShapeEngine.Core.CollisionSystem;
 using ShapeEngine.Lib;
 
 namespace ShapeEngine.Core.Structs;
@@ -29,11 +30,131 @@ public readonly struct CollisionPoint : IEquatable<CollisionPoint>
     #endregion
     
     #region Public Functions
-    public CollisionPoint Average(CollisionPoint other) => new((Point + other.Point) / 2, (Normal + other.Normal).Normalize());
-    
-    public static CollisionPoint Average(CollisionPoint a, CollisionPoint b) => new((a.Point + b.Point) / 2, (a.Normal + b.Normal).Normalize());
+    /*
+    public static CollisionPoint GetAggregatedCollisionPoint(CollisionPoint cur, CollisionPoint next,  CollisionPointAggregationType aggregationType, Vector2 referenceV, float value, out float newValue)
+    {
+        newValue = 0f;
+        if (!cur.Valid) return next;
+        if (!next.Valid) return cur;
+        
+        switch (aggregationType)
+        {
+            case CollisionPointAggregationType.First: return new();
+            
+            case CollisionPointAggregationType.Closest:
+                var minDisSquared = (next.Point - referenceV).LengthSquared();
+                if (minDisSquared < value || value < 0)
+                {
+                    newValue = minDisSquared;
+                    return next;
+                }
 
-    public static CollisionPoint Average(params CollisionPoint[] points)
+                return cur;
+                
+            case CollisionPointAggregationType.Furthest:
+                var maxDisSquared = (next.Point - referenceV).LengthSquared();
+                if (maxDisSquared > value || value < 0)
+                {
+                    newValue = maxDisSquared;
+                    return next;
+                }
+
+                return cur;
+            
+            case CollisionPointAggregationType.Combined: return cur.Combine(next);
+            
+            case CollisionPointAggregationType.PointingTowards:
+                float minDot = next.Normal.Dot(referenceV);
+                if (minDot < value || value == 0f)
+                {
+                    newValue = minDot;
+                    return next;
+                }
+                return cur;
+            case CollisionPointAggregationType.PointingAway:
+                float maxDot = next.Normal.Dot(referenceV);
+                if (maxDot > value || value == 0f)
+                {
+                    newValue = maxDot;
+                    return next;
+                }
+                return cur;;
+        }
+
+        return new();
+    }
+    */
+
+    public static bool IsCloser(CollisionPoint p, Vector2 referencePoint, float curMinDisSquared, out float newMinDisSquared)
+    {
+        var disSquared = (p.Point - referencePoint).LengthSquared();
+        if (disSquared < curMinDisSquared || curMinDisSquared < 0)
+        {
+            newMinDisSquared = disSquared;
+            return true;
+        }
+        
+        newMinDisSquared = curMinDisSquared;
+        return false;
+    }
+    public static bool IsFurther(CollisionPoint p, Vector2 referencePoint, float curMaxDisSquared, out float newMaxDisSquared)
+    {
+        var disSquared = (p.Point - referencePoint).LengthSquared();
+        if (disSquared > curMaxDisSquared || curMaxDisSquared < 0)
+        {
+            newMaxDisSquared = disSquared;
+            return true;
+        }
+        
+        newMaxDisSquared = curMaxDisSquared;
+        return false;
+    }
+
+    /// <summary>
+    /// Returns true if the reference direction is pointing in the same direction as the normal of the collision point. Dot values greater than 0 mean pointing towards.
+    /// </summary>
+    /// <param name="p">The collision point to check.</param>
+    /// <param name="referenceDir">The reference direction to check against.</param>
+    /// <param name="curDot">The cur maximum dot value from the previous collision points. If 0 returns true automatically with the new dot.</param>
+    /// <param name="newDot">The new maximum dot value. If the normal of p is pointing more in the same direction of the reference direction than cur dot suggests. </param>
+    /// <returns></returns>
+    public static bool IsPointingTowards(CollisionPoint p, Vector2 referenceDir, float curDot, out float newDot)
+    {
+        var dot = p.Normal.Dot(referenceDir);
+        if (dot > curDot || curDot == 0f)
+        {
+            newDot = dot;
+            return true;
+        }
+        
+        newDot = curDot;
+        return false;
+    }
+    /// <summary>
+    /// Returns true if the reference direction is pointing in the opposite  direction as the normal of the collision point. Dot values smaller than 0 mean pointing away.
+    /// </summary>
+    /// <param name="p">The collision point to check.</param>
+    /// <param name="referenceDir">The reference direction to check against.</param>
+    /// <param name="curDot">The cur minimum dot value from the previous collision points. If 0 return true automatically with the new dot.</param>
+    /// <param name="newDot">The new minimum dot value. If the normal from p is pointing more in the opposite direction of the reference direction than cur dot suggests.</param>
+    /// <returns></returns>
+    public static bool IsPointingAway(CollisionPoint p, Vector2 referenceDir, float curDot, out float newDot)
+    {
+        var dot = p.Normal.Dot(referenceDir);
+        if (dot < curDot || curDot == 0f)
+        {
+            newDot = dot;
+            return true;
+        }
+        
+        newDot = curDot;
+        return false;
+    }
+    public CollisionPoint Combine(CollisionPoint other) => new((Point + other.Point) / 2, (Normal + other.Normal).Normalize());
+    
+    public static CollisionPoint Combine(CollisionPoint a, CollisionPoint b) => new((a.Point + b.Point) / 2, (a.Normal + b.Normal).Normalize());
+
+    public static CollisionPoint Combine(params CollisionPoint[] points)
     {
         if(points.Length == 0) return new();
         var avgPoint = Vector2.Zero;
