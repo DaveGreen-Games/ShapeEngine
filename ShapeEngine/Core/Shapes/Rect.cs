f074897faedc0f9c8ac3a44418d8e15c5fe78280
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-01-20 08:19:57 +0100

    Rect Contains overhaul finished.

--- ShapeEngine/Core/Shapes/Rect.cs
+++ ShapeEngine/Core/Shapes/Rect.cs
@@ -2267,8 +2267,127 @@ public readonly struct Rect : IEquatable<Rect>
     #endregion
     
     #region Contains
+    public static bool ContainsRectPoint(Vector2 topLeft, Vector2 bottomRight, Vector2 point)
+    {
+        var left = topLeft.X;
+        var right = bottomRight.X;
+        var top = topLeft.Y;
+        var bottom = bottomRight.Y;
+        return left <= point.X && right >= point.X && top <= point.Y && bottom >= point.Y;
+    }
+    public static bool ContainsRectPoints(Vector2 topLeft, Vector2 bottomRight, Vector2 u, Vector2 v)
+    {
+        var left = topLeft.X;
+        var right = bottomRight.X;
+        var top = topLeft.Y;
+        var bottom = bottomRight.Y;
+        
+        return left <= u.X && right >= u.X && top <= u.Y && bottom >= u.Y && 
+               left <= v.X && right >= v.X && top <= v.Y && bottom >= v.Y ;
+    }
+    public static bool ContainsRectPoints(Vector2 topLeft, Vector2 bottomRight, Vector2 u, Vector2 v, Vector2 w)
+    {
+        var left = topLeft.X;
+        var right = bottomRight.X;
+        var top = topLeft.Y;
+        var bottom = bottomRight.Y;
+        
+        return left <= u.X && right >= u.X && top <= u.Y && bottom >= u.Y && 
+               left <= v.X && right >= v.X && top <= v.Y && bottom >= v.Y &&
+               left <= w.X && right >= w.X && top <= w.Y && bottom >= w.Y ;
+    }
+    public static bool ContainsRectPoints(Vector2 topLeft, Vector2 bottomRight, Vector2 u, Vector2 v, Vector2 w, Vector2 x)
+    {
+        var left = topLeft.X;
+        var right = bottomRight.X;
+        var top = topLeft.Y;
+        var bottom = bottomRight.Y;
+        
+        return left <= u.X && right >= u.X && top <= u.Y && bottom >= u.Y && 
+               left <= v.X && right >= v.X && top <= v.Y && bottom >= v.Y &&
+               left <= w.X && right >= w.X && top <= w.Y && bottom >= w.Y &&
+               left <= x.X && right >= x.X && top <= x.Y && bottom >= x.Y ;
+    }
+    public static bool ContainsRectPoints(Vector2 topLeft, Vector2 bottomRight, List<Vector2> points)
+    {
+        if (points.Count <= 0) return false;
+        
+        var left = topLeft.X;
+        var right = bottomRight.X;
+        var top = topLeft.Y;
+        var bottom = bottomRight.Y;
+        
+        foreach (var point in points)
+        {
+            var contains = left <= point.X && right >= point.X && top <= point.Y && bottom >= point.Y;
+            if (!contains) return false;
+        }
 
-    public bool ContainsPoint(Vector2 p) => Left <= p.X && Right >= p.X && Top <= p.Y && Bottom >= p.Y;
+        return true;
+    }
+    
+    public static bool ContainsRectSegment(Vector2 topLeft, Vector2 bottomRight, Vector2 segmentStart, Vector2 segmentEnd)
+    {
+        return ContainsRectPoints(topLeft, bottomRight, segmentStart, segmentEnd);
+    }
+    public static bool ContainsRectCircle(Vector2 topLeft, Vector2 bottomRight, Vector2 circleCenter, float circleRadius)
+    {
+        if (!ContainsRectPoint(topLeft, bottomRight, circleCenter)) return false;
+        var a = topLeft;
+        var b = new Vector2(topLeft.X, bottomRight.Y);
+        var c = bottomRight;
+        var d = new Vector2(bottomRight.X, topLeft.Y);
+        var result = Segment.IntersectSegmentCircle(a, b, circleCenter, circleRadius);
+        if (result.a.Valid || result.b.Valid) return false;
+            
+        result = Segment.IntersectSegmentCircle(b, c, circleCenter, circleRadius);
+        if (result.a.Valid || result.b.Valid) return false;
+        
+        result = Segment.IntersectSegmentCircle(c, d, circleCenter, circleRadius);
+        if (result.a.Valid || result.b.Valid) return false;
+
+        result = Segment.IntersectSegmentCircle(d, a, circleCenter, circleRadius);
+        if (result.a.Valid || result.b.Valid) return false;
+        return true;
+
+    }
+    public static bool ContainsRectTriangle(Vector2 topLeft, Vector2 bottomRight, Vector2 tA, Vector2 tB, Vector2 tc)
+    {
+       return ContainsRectPoints(topLeft, bottomRight, tA, tB, tc);
+    }
+    public static bool ContainsRectQuad(Vector2 topLeft, Vector2 bottomRight, Vector2 qA, Vector2 qB, Vector2 qC, Vector2 qD)
+    {
+        return ContainsRectPoints(topLeft, bottomRight, qA, qB, qC, qD);
+    }
+    public static bool ContainsRectRect(Vector2 topLeft1, Vector2 bottomRight1, Vector2 topLeft2, Vector2 bottomRight2)
+    {
+        var x = topLeft1.X;
+        var y = topLeft1.Y;
+        var width = bottomRight1.X - x;
+        var height = bottomRight1.Y - x;
+        
+        var otherX = topLeft2.X;
+        var otherY = topLeft2.Y;
+        var otherWidth = bottomRight2.X - otherX;
+        var otherHeight = bottomRight2.Y - otherY;
+        return (x <= otherX) && (otherX + otherWidth <= x + width) &&
+               (y <= otherY) && (otherY + otherHeight <= y + height);
+        // return ContainsRectPoints(rA1, rB1, rC1, rD1, rA2, rB2, rC2, rD2);
+    }
+    public static bool ContainsRectPolyline(Vector2 topLeft, Vector2 bottomRight,  List<Vector2> polyline)
+    {
+        return ContainsRectPoints(topLeft, bottomRight, polyline);
+    }
+    public static bool ContainsRectPolygon(Vector2 topLeft, Vector2 bottomRight, List<Vector2> polygon)
+    {
+        return ContainsRectPoints(topLeft, bottomRight, polygon);
+    }
+        
+    public bool ContainsPoint(Vector2 p) => ContainsRectPoint(TopLeft, BottomRight, p);
+    public bool ContainsPoints(Vector2 u, Vector2 v) => ContainsRectPoints(TopLeft, BottomRight, u, v);
+    public bool ContainsPoints(Vector2 u, Vector2 v, Vector2 w) => ContainsRectPoints(TopLeft, BottomRight, u, v, w);
+    public bool ContainsPoints(Vector2 u, Vector2 v, Vector2 w, Vector2 x) => ContainsRectPoints(TopLeft, BottomRight, u, v, w, x);
+    public bool ContainsPoints(List<Vector2> points) => ContainsRectPoints(TopLeft, BottomRight, points);
     
     public bool ContainsCollisionObject(CollisionObject collisionObject)
     {
@@ -2301,47 +2420,35 @@ public readonly struct Rect : IEquatable<Rect>
     //     (Y <= rect.Y) && (rect.Y + rect.Height <= Y + Height);
     public bool ContainsShape(Segment segment)
     {
-        return ContainsPoint(segment.Start) && ContainsPoint(segment.End);
+        return ContainsRectPoints(TopLeft, BottomRight, segment.Start, segment.End);
     }
     public bool ContainsShape(Circle circle)
     {
-        return ContainsPoint(circle.Top) &&
-               ContainsPoint(circle.Left) &&
-               ContainsPoint(circle.Bottom) &&
-               ContainsPoint(circle.Right);
+        return ContainsRectCircle(TopLeft, BottomRight, circle.Center, circle.Radius);
     }
     public bool ContainsShape(Rect rect)
     {
-        return (X <= rect.X) && (rect.X + rect.Width <= X + Width) &&
-            (Y <= rect.Y) && (rect.Y + rect.Height <= Y + Height);
-        // return this.X <= other.X && other.X + other.Width <= this.X + this.Width && this.Y <= other.Y && other.Y + other.Height <= this.Y + this.Height;
-        // return ContainsPoint(other.TopLeft) &&
-        //     ContainsPoint(other.BottomLeft) &&
-        //     ContainsPoint(other.BottomRight) &&
-        //     ContainsPoint(other.TopRight);
+        return ContainsRectRect(TopLeft, BottomRight, rect.TopLeft, rect.BottomRight);
     }
     public bool ContainsShape(Triangle triangle)
     {
-        return ContainsPoint(triangle.A) &&
-            ContainsPoint(triangle.B) &&
-            ContainsPoint(triangle.C);
+        return ContainsRectPoints(TopLeft, BottomRight, triangle.A, triangle.B, triangle.C);
     }
     public bool ContainsShape(Quad quad)
     {
-        return ContainsPoint(quad.A) &&
-               ContainsPoint(quad.B) &&
-               ContainsPoint(quad.C) &&
-               ContainsPoint(quad.D);
+        return ContainsRectPoints(TopLeft, BottomRight, quad.A, quad.B, quad.C, quad.D);
     }
-    
+    public bool ContainsShape(Polyline polyline)
+    {
+        return ContainsRectPoints(TopLeft, BottomRight, polyline);
+    }
+    public bool ContainsShape(Polygon polygon)
+    {
+        return ContainsRectPoints(TopLeft, BottomRight, polygon);
+    }
     public bool ContainsShape(Points points)
     {
-        if (points.Count <= 0) return false;
-        foreach (var p in points)
-        {
-            if (!ContainsPoint(p)) return false;
-        }
-        return true;
+        return ContainsRectPoints(TopLeft, BottomRight, points);
     }
 
 
