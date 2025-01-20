fe5c2b9e759fd3793c06c99dbe1a3b11b5438234
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-01-20 08:27:00 +0100

    Triangle Contains functions parameters renamed to match rect.

--- ShapeEngine/Core/Shapes/Triangle.cs
+++ ShapeEngine/Core/Shapes/Triangle.cs
@@ -703,15 +703,15 @@ public readonly struct Triangle : IEquatable<Triangle>
 
     #region Contains
 
-    public static bool ContainsTrianglePoint(Vector2 a, Vector2 b, Vector2 c, Vector2 point)
+    public static bool ContainsTrianglePoint(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 point)
     {
-        var ab = b - a;
-        var bc = c - b;
-        var ca = a - c;
+        var ab = tB - tA;
+        var bc = tC - tB;
+        var ca = tA - tC;
 
-        var ap = point - a;
-        var bp = point - b;
-        var cp = point - c;
+        var ap = point - tA;
+        var bp = point - tB;
+        var cp = point - tC;
 
         float c1 = ab.Cross(ap);
         float c2 = bc.Cross(bp);
@@ -719,76 +719,74 @@ public readonly struct Triangle : IEquatable<Triangle>
 
         return c1 < 0f && c2 < 0f && c3 < 0f;
     }
-    public static bool ContainsTrianglePoints(Vector2 a, Vector2 b, Vector2 c, Vector2 u, Vector2 v)
+    public static bool ContainsTrianglePoints(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 u, Vector2 v)
     {
-        return ContainsTrianglePoint(a,b,c, u) && 
-               ContainsTrianglePoint(a,b,c, v);
+        return ContainsTrianglePoint(tA, tB, tC, u) && 
+               ContainsTrianglePoint(tA, tB, tC, v);
     }
-    public static bool ContainsTrianglePoints(Vector2 a, Vector2 b, Vector2 c, Vector2 u, Vector2 v, Vector2 w)
+    public static bool ContainsTrianglePoints(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 u, Vector2 v, Vector2 w)
     {
-        return ContainsTrianglePoint(a, b, c, u) && 
-               ContainsTrianglePoint(a, b, c, v) && 
-               ContainsTrianglePoint(a, b, c, w);
+        return ContainsTrianglePoint(tA, tB, tC, u) && 
+               ContainsTrianglePoint(tA, tB, tC, v) && 
+               ContainsTrianglePoint(tA, tB, tC, w);
     }
-    public static bool ContainsTrianglePoints(Vector2 a, Vector2 b, Vector2 c, Vector2 u, Vector2 v, Vector2 w, Vector2 x)
+    public static bool ContainsTrianglePoints(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 u, Vector2 v, Vector2 w, Vector2 x)
     {
-        return ContainsTrianglePoint(a, b, c, u) && 
-               ContainsTrianglePoint(a, b, c, v) && 
-               ContainsTrianglePoint(a, b, c, w) && 
-               ContainsTrianglePoint(a, b, c, x);
+        return ContainsTrianglePoint(tA, tB, tC, u) && 
+               ContainsTrianglePoint(tA, tB, tC, v) && 
+               ContainsTrianglePoint(tA, tB, tC, w) && 
+               ContainsTrianglePoint(tA, tB, tC, x);
     }
-    public static bool ContainsTrianglePoints(Vector2 a, Vector2 b, Vector2 c, List<Vector2> points)
+    public static bool ContainsTrianglePoints(Vector2 tA, Vector2 tB, Vector2 tC, List<Vector2> points)
     {
         if (points.Count <= 0) return false;
         foreach (var point in points)
         {
-            if (!ContainsTrianglePoint(a, b, c, point)) return false;
+            if (!ContainsTrianglePoint(tA, tB, tC, point)) return false;
         }
-
         return true;
     }
   
     
-    public static bool ContainsTriangleSegment(Vector2 a, Vector2 b, Vector2 c, Vector2 segmentStart, Vector2 segmentEnd)
+    public static bool ContainsTriangleSegment(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 segmentStart, Vector2 segmentEnd)
     {
-        if(!ContainsTrianglePoints(a, b, c, segmentStart, segmentEnd)) return false;
-        return true;
+        return ContainsTrianglePoints(tA, tB, tC, segmentStart, segmentEnd);
     }
-    public static bool ContainsTriangleCircle(Vector2 a, Vector2 b, Vector2 c, Vector2 circleCenter, float circleRadius)
+    public static bool ContainsTriangleCircle(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 circleCenter, float circleRadius)
     {
-        if (!ContainsTrianglePoint(a, b, c, circleCenter)) return false;
+        if (!ContainsTrianglePoint(tA, tB, tC, circleCenter)) return false;
         
-        var result = Segment.IntersectSegmentCircle(a, b, circleCenter, circleRadius);
+        var result = Segment.IntersectSegmentCircle(tA, tB, circleCenter, circleRadius);
         if (result.a.Valid || result.b.Valid) return false;
             
-        result = Segment.IntersectSegmentCircle(b, c, circleCenter, circleRadius);
+        result = Segment.IntersectSegmentCircle(tB, tC, circleCenter, circleRadius);
         if (result.a.Valid || result.b.Valid) return false;
         
-        result = Segment.IntersectSegmentCircle(c, a, circleCenter, circleRadius);
+        result = Segment.IntersectSegmentCircle(tC, tA, circleCenter, circleRadius);
         if (result.a.Valid || result.b.Valid) return false;
 
         return true;
 
     }
-    public static bool ContainsTriangleTriangle(Vector2 a1, Vector2 b1, Vector2 c1, Vector2 a2, Vector2 b2, Vector2 c2)
+    public static bool ContainsTriangleTriangle(Vector2 tA1, Vector2 tB1, Vector2 tC1, Vector2 tA2, Vector2 tB2, Vector2 tC2)
     {
-       return ContainsTrianglePoints(a1, b1, c1, a2, b2, c2);
+       return ContainsTrianglePoints(tA1, tB1, tC1, tA2, tB2, tC2);
     }
-    public static bool ContainsTriangleQuad(Vector2 a, Vector2 b, Vector2 c, Vector2 u, Vector2 v, Vector2 w, Vector2 x)
+    public static bool ContainsTriangleQuad(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 qA, Vector2 qB, Vector2 qC, Vector2 qD)
     {
-        return ContainsTrianglePoints(a, b, c, u, v, w, x);
+        return ContainsTrianglePoints(tA, tB, tC, qA, qB, qC, qD);
     }
-    public static bool ContainsTriangleRect(Vector2 a, Vector2 b, Vector2 c, Vector2 u, Vector2 v, Vector2 w, Vector2 x)
+    public static bool ContainsTriangleRect(Vector2 tA, Vector2 tB, Vector2 tC, Vector2 rA, Vector2 rB, Vector2 rC, Vector2 rD)
     {
-        return ContainsTrianglePoints(a, b, c, u, v, w, x);
+        return ContainsTrianglePoints(tA, tB, tC, rA, rB, rC, rD);
     }
-    public static bool ContainsTrianglePolyline(Vector2 a, Vector2 b, Vector2 c, List<Vector2> polyline)
+    public static bool ContainsTrianglePolyline(Vector2 tA, Vector2 tB, Vector2 tC, List<Vector2> polyline)
     {
-        return ContainsTrianglePoints(a, b, c, polyline);
+        return ContainsTrianglePoints(tA, tB, tC, polyline);
     }
-    public static bool ContainsTrianglePolygon(Vector2 a, Vector2 b, Vector2 c, List<Vector2> polygon)
+    public static bool ContainsTrianglePolygon(Vector2 tA, Vector2 tB, Vector2 tC, List<Vector2> polygon)
     {
-        return ContainsTrianglePoints(a, b, c, polygon);
+        return ContainsTrianglePoints(tA, tB, tC, polygon);
     }
         
     public bool ContainsPoint(Vector2 p) => ContainsTrianglePoint(A, B, C, p);
