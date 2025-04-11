1145a5800edfa477d3bcb081e8105faf41299c80
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-04-11 12:18:46 +0200

    FractureSplit function implemented.

--- ShapeEngine/Core/Shapes/Polygon.cs
+++ ShapeEngine/Core/Shapes/Polygon.cs
@@ -825,6 +825,7 @@ namespace ShapeEngine.Core.Shapes
             var cut = Generate(cutLine, minMagnitude, maxMagnitude, minSectionLength, maxSectionLength);
             return this.CutShape(cut);
         }
+        
         public Polygons? Split(Vector2 point, Vector2 direction)
         {
             var line = new Line(point, direction);
@@ -853,33 +854,75 @@ namespace ShapeEngine.Core.Shapes
             return result.ToPolygons();
         }
 
-        public Segments? GenerateFractureLine(Vector2 start, Vector2 end, int segmentPoints)
+        /// <summary>
+        /// Generates a facture line in the direction of dir and splits the polygon with it.
+        /// </summary>
+        /// <param name="dir">The direction for the fracture.</param>
+        /// <param name="maxOffsetPercentage">Max distance each point can be offset along the fracture line.
+        /// Value Range 0-1.
+        /// Relative to segment length of the fracture line.</param>
+        /// <param name="fractureLineComplexity">How many points should be generated for the fracture line.</param>
+        /// <returns></returns>
+        public Polygons? FractureSplit(Vector2 dir, float maxOffsetPercentage,  int fractureLineComplexity)
         {
-            if (segmentPoints < 1) return null;
+            if(fractureLineComplexity < 1 || dir.LengthSquared() <= 0f) return null;
+            
+            var center = Center;
+            var result = IntersectShape(new Line(center, dir));
+            if (result == null || result.Count < 2) return null;
+
+            var start = result[0].Point;
+            var end = result[1].Point;
+            if (result.Count > 2)
+            {
+                end = result.GetFurthestCollisionPoint(start).Point;
+            }
+            
+            var fractureLine = GenerateFractureLine(start, end, maxOffsetPercentage, fractureLineComplexity);
+            if(fractureLine == null) return null;
+            return Split(fractureLine);
+        }
+        
+        /// <summary>
+        /// Generate a fracture line from start to end.
+        /// </summary>
+        /// <param name="start">Start of the fracture line.</param>
+        /// <param name="end">End of the fracture line.</param>
+        /// <param name="maxOffsetPercentage">How far each point can be offset from the main line.
+        /// Value Range 0-1.
+        /// Relative to the segment length.</param>
+        /// <param name="linePoints">How many points should be generated. Final segment count = segmentPoints + 1.</param>
+        /// <returns></returns>
+        public Segments? GenerateFractureLine(Vector2 start, Vector2 end, float maxOffsetPercentage, int linePoints)
+        {
+            if (linePoints < 1) return null;
+            if (maxOffsetPercentage < 0f) return [new Segment(start, end)];
             
             var w = end - start;
             var disSquared = w.LengthSquared();
             if (disSquared <= 0f) return null;
             
-            var p = w.GetPerpendicularLeft();
             var l = MathF.Sqrt(disSquared);
-            var segmentLength = l / (segmentPoints + 1);
+            var segmentLength = l / (linePoints + 1);
             var dir = w / l;
+            var p = dir.GetPerpendicularLeft();
             var result = new Segments();
 
             var curStart = start;
             var curLinePoint = start;
-            for (int i = 0; i < segmentPoints; i++)
+            for (int i = 0; i < linePoints; i++)
             {
                 var point = curStart + dir * segmentLength;
                 curStart = point;
                 
-                var offset = p;
+                var offsetLength = Rng.Instance.RandF(0f, segmentLength * maxOffsetPercentage);
+                var offset = p * offsetLength;
                 if (Rng.Instance.Chance(0.5f))
                 {
-                    offset = -p;
+                    offset = -p * offsetLength;
                 }
 
+         
                 var nextLinePoint = point + offset;
 
                 var segment = new Segment(curLinePoint, nextLinePoint);
