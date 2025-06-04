2f5dd7cedbdae9b655345879ff406612955377c3
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-06-04 17:06:13 +0200

    Docs: ExitCode.cs summaries / documentation added.

--- ShapeEngine/Core/Structs/ExitCode.cs
+++ ShapeEngine/Core/Structs/ExitCode.cs
@@ -1,11 +1,20 @@
 namespace ShapeEngine.Core.Structs;
 
 /// <summary>
-/// Returned by the Run() function in the GameLoop class.
+/// Represents an exit code returned by the <see cref="Game.Run"/> function in the Game class.
 /// </summary>
 public readonly struct ExitCode
 {
+    /// <summary>
+    /// Gets a value indicating whether the game should restart.
+    /// </summary>
+    /// <value><c>true</c> if the game should restart; otherwise, <c>false</c>.</value>
     public readonly bool Restart = false;
-    public ExitCode(bool restart) { this.Restart = restart; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExitCode"/> struct.
+    /// </summary>
+    /// <param name="restart">A value indicating whether the game should restart. 
+    /// If <c>true</c>, the game will restart; if <c>false</c>, the game will exit.</param>
+    public ExitCode(bool restart) { this.Restart = restart; }
 }
