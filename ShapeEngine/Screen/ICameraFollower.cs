e0ceaeba66c2583475af6cd26b69e92f7fec028a
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-06-06 09:50:38 +0200

    Docs: Added XML summaries for ICameraFollower interface methods

--- ShapeEngine/Screen/ICameraFollower.cs
+++ ShapeEngine/Screen/ICameraFollower.cs
@@ -3,15 +3,31 @@ using ShapeEngine.StaticLib;
 
 namespace ShapeEngine.Screen;
 
-
+/// <summary>
+/// Defines methods for objects that follow or interact with a camera in the scene.
+/// </summary>
 public interface ICameraFollower
 {
+    /// <summary>
+    /// Resets the follower to its initial state.
+    /// </summary>
     public void Reset();
+
+    /// <summary>
+    /// Updates the follower's state based on the elapsed time and current camera rectangle.
+    /// </summary>
+    /// <param name="dt">The elapsed time since the last update.</param>
+    /// <param name="cameraRect">The current rectangle representing the camera's view.</param>
+    /// <returns>The updated rectangle for the follower.</returns>
     public Rect Update(float dt, Rect cameraRect);
-    
-    
+
+    /// <summary>
+    /// Called when the follower is attached to a camera.
+    /// </summary>
     public void OnCameraAttached();
+
+    /// <summary>
+    /// Called when the follower is detached from a camera.
+    /// </summary>
     public void OnCameraDetached();
-
-
 }
