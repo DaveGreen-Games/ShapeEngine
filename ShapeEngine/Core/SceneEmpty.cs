a217654670f695c4fbbbb15b7f1343b82fe68040
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-06-17 10:40:13 +0200

    Docs: Added XML summaries to SceneEmpty class to clarify its purpose and methods

--- ShapeEngine/Core/SceneEmpty.cs
+++ ShapeEngine/Core/SceneEmpty.cs
@@ -1,18 +1,30 @@
-﻿using ShapeEngine.Core.Interfaces;
+﻿
+namespace ShapeEngine.Core;
 
-namespace ShapeEngine.Core
+/// <summary>
+/// Represents an empty scene with no additional logic.
+/// </summary>
+public sealed class SceneEmpty : Scene
 {
-    public sealed class SceneEmpty : Scene
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SceneEmpty"/> class.
+    /// </summary>
+    public SceneEmpty() { }
+
+    /// <summary>
+    /// Called when the scene is activated.
+    /// </summary>
+    /// <param name="oldScene">The previously active scene.</param>
+    protected override void OnActivate(Scene oldScene)
     {
-        public SceneEmpty() { }
-
-        protected override void OnActivate(Scene oldScene)
-        {
-        }
-
-        protected override void OnDeactivate()
-        {
-        }
     }
 
+    /// <summary>
+    /// Called when the scene is deactivated.
+    /// </summary>
+    protected override void OnDeactivate()
+    {
+    }
 }
+
+
