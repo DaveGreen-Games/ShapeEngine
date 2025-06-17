ba277f88bc9a1f4b0e3175097de06c9ff9dd4233
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-06-17 10:37:54 +0200

    Docs: Added XML summaries to DeferredInfo class for improved clarity on functionality and parameters

--- ShapeEngine/Core/DeferredInfo.cs
+++ ShapeEngine/Core/DeferredInfo.cs
@@ -1,15 +1,37 @@
 namespace ShapeEngine.Core;
 
+/// <summary>
+/// Represents a deferred action that will be executed after a specified number of frames.
+/// </summary>
 internal class DeferredInfo
 {
+    /// <summary>
+    /// The action to be executed after the delay.
+    /// </summary>
     private readonly Action action;
+
+    /// <summary>
+    /// The number of frames to wait before executing the action.
+    /// </summary>
     private int frames;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeferredInfo"/> class.
+    /// </summary>
+    /// <param name="action">The action to execute after the delay.</param>
+    /// <param name="frames">The number of frames to wait before execution.</param>
     public DeferredInfo(Action action, int frames)
     {
         this.action = action;
         this.frames = frames;
     }
 
+    /// <summary>
+    /// Decrements the frame counter and invokes the action if the delay has elapsed.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> if the action was invoked; otherwise, <c>false</c>.
+    /// </returns>
     public bool Call()
     {
         if (frames <= 0)
@@ -17,11 +39,9 @@ internal class DeferredInfo
             action.Invoke();
             return true;
         }
-        else
-        {
-            frames--;
-            return false;
-        }
+        
+        frames--;
+        return false;
     }
 
 }
