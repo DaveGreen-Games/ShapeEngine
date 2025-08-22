cec7ece7e22942f209625b934df09bd99b733a01
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-08-22 16:29:27 +0200

    Feat: Added functions for Serializing/Deserializing multiple objects at once.

--- ShapeEngine/Serialization/JsonClassSerializer.cs
+++ ShapeEngine/Serialization/JsonClassSerializer.cs
@@ -45,17 +45,128 @@ public class JsonClassSerializer<T> where T : class
     {
         this.options = options;
     }
+ 
     /// <summary>
     /// Serializes the specified object to a JSON string.
     /// </summary>
-    /// <param name="enemy">The object to serialize.</param>
+    /// <param name="instance">The object to serialize.</param>
     /// <returns>A JSON string representation of the object.</returns>
-    public string Serialize(T enemy) => JsonSerializer.Serialize(enemy, options);
-
+    public string Serialize(T instance) => JsonSerializer.Serialize(instance, options);
+    /// <summary>
+    /// Serializes a list of objects to a list of JSON strings.
+    /// </summary>
+    /// <param name="instances">The list of objects to serialize.</param>
+    /// <returns>A list of JSON string representations of the objects.</returns>
+    public List<string> Serialize(List<T> instances)
+    {
+        var list = new List<string>(instances.Count);
+        foreach (var instance in instances)
+        {
+            list.Add(JsonSerializer.Serialize(instance, options));
+        }
+        return list;
+    }
+   
     /// <summary>
     /// Deserializes the specified JSON string to an object of type <typeparamref name="T"/>.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized object, or null if deserialization fails.</returns>
     public T? Deserialize(string json) => JsonSerializer.Deserialize<T>(json, options);
+    /// <summary>
+    /// Deserializes a list of JSON strings to a list of objects of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="jsons">The list of JSON strings to deserialize.</param>
+    /// <returns>A list of deserialized objects of type <typeparamref name="T"/>.</returns>
+    public List<T> Deserialize(List<string> jsons)
+    {
+        var list = new List<T>(jsons.Count);
+        foreach (var json in jsons)
+        {
+            var obj = JsonSerializer.Deserialize<T>(json, options);
+            if (obj != null) list.Add(obj);
+        }
+        return list;
+    }
+    
+    /// <summary>
+    /// Serializes the specified object to a JSON string using optional serializer options.
+    /// </summary>
+    /// <typeparam name="TS">The type of the object to serialize.</typeparam>
+    /// <param name="instance">The object to serialize.</param>
+    /// <param name="options">Optional serializer options. If null, default options are used.</param>
+    /// <returns>A JSON string representation of the object.</returns>
+    public static string SerializeStatic<TS>(TS instance, JsonSerializerOptions? options = null) where TS : class
+    {
+        options ??= new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = null,
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+        return JsonSerializer.Serialize(instance, options);
+    }
+    /// <summary>
+    /// Serializes a list of objects to a list of JSON strings using optional serializer options.
+    /// </summary>
+    /// <typeparam name="TS">The type of the objects to serialize.</typeparam>
+    /// <param name="instances">The list of objects to serialize.</param>
+    /// <param name="options">Optional serializer options. If null, default options are used.</param>
+    /// <returns>A list of JSON string representations of the objects.</returns>
+    public static List<string> SerializeStatic<TS>(List<TS> instances, JsonSerializerOptions? options = null) where TS : class
+    {
+        options ??= new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = null,
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+        var list = new List<string>(instances.Count);
+        foreach (var instance in instances)
+        {
+            list.Add(JsonSerializer.Serialize(instance, options));
+        }
+        return list;
+    }
+    
+    /// <summary>
+    /// Deserializes the specified JSON string to an object of type <typeparamref name="TS"/> using optional serializer options.
+    /// </summary>
+    /// <typeparam name="TS">The type of the object to deserialize.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="options">Optional serializer options. If null, default options are used.</param>
+    /// <returns>The deserialized object, or null if deserialization fails.</returns>
+    public static TS? DeserializeStatic<TS>(string json, JsonSerializerOptions? options = null) where TS : class
+    {
+        options ??= new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = null,
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+        return JsonSerializer.Deserialize<TS>(json, options);
+    }
+    /// <summary>
+    /// Deserializes a list of JSON strings to a list of objects of type <typeparamref name="TS"/> using optional serializer options.
+    /// </summary>
+    /// <typeparam name="TS">The type of the objects to deserialize.</typeparam>
+    /// <param name="jsons">The list of JSON strings to deserialize.</param>
+    /// <param name="options">Optional serializer options. If null, default options are used.</param>
+    /// <returns>A list of deserialized objects of type <typeparamref name="TS"/>.</returns>
+    public static List<TS> SerializeStatic<TS>(List<string> jsons, JsonSerializerOptions? options = null) where TS : class
+    {
+        options ??= new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = null,
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+        var list = new List<TS>(jsons.Count);
+        foreach (var json in jsons)
+        {
+            var obj = JsonSerializer.Deserialize<TS>(json, options);
+            if (obj != null) list.Add(obj);
+        }
+        return list;
+    }
 }
