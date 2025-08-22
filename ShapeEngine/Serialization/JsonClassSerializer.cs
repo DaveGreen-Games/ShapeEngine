f75866b593df3c3bdc5a3b3c7b2a22e392838155
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-08-22 14:39:59 +0200

    Feat: Enhance JsonClassSerializer and JsonStructSerializer with detailed XML documentation

--- ShapeEngine/Serialization/JsonClassSerializer.cs
+++ ShapeEngine/Serialization/JsonClassSerializer.cs
@@ -3,10 +3,16 @@ using System.Text.Json.Serialization;
 
 namespace ShapeEngine.Serialization;
 
+/// <summary>
+/// Provides serialization and deserialization of objects to and from JSON using customizable options.
+/// </summary>
 public class JsonClassSerializer<T> where T : class
 {
     private readonly JsonSerializerOptions options;
     
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonClassSerializer{T}"/> with default options (camel case, indented, enum as string).
+    /// </summary>
     public JsonClassSerializer()
     {
         options = new()
@@ -16,8 +22,39 @@ public class JsonClassSerializer<T> where T : class
             Converters = { new JsonStringEnumConverter() }
         };
     }
-    
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonClassSerializer{T}"/> with a custom naming policy and indentation option.
+    /// </summary>
+    /// <param name="namingPolicy">The property naming policy to use.</param>
+    /// <param name="writeIndented">Whether to write indented JSON.</param>
+    public JsonClassSerializer(JsonNamingPolicy namingPolicy, bool writeIndented = true)
+    {
+        options = new()
+        {
+            PropertyNamingPolicy = namingPolicy,
+            WriteIndented = writeIndented,
+            Converters = { new JsonStringEnumConverter() }
+        };
+    }
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonClassSerializer{T}"/> with custom <see cref="JsonSerializerOptions"/>.
+    /// </summary>
+    /// <param name="options">The serializer options to use.</param>
+    public JsonClassSerializer(JsonSerializerOptions options)
+    {
+        this.options = options;
+    }
+    /// <summary>
+    /// Serializes the specified object to a JSON string.
+    /// </summary>
+    /// <param name="enemy">The object to serialize.</param>
+    /// <returns>A JSON string representation of the object.</returns>
     public string Serialize(T enemy) => JsonSerializer.Serialize(enemy, options);
 
+    /// <summary>
+    /// Deserializes the specified JSON string to an object of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized object, or null if deserialization fails.</returns>
     public T? Deserialize(string json) => JsonSerializer.Deserialize<T>(json, options);
 }
--- ShapeEngine/Serialization/JsonStructSerializer.cs
+++ ShapeEngine/Serialization/JsonStructSerializer.cs
@@ -3,10 +3,16 @@ using System.Text.Json.Serialization;
 
 namespace ShapeEngine.Serialization;
 
+/// <summary>
+/// Provides serialization and deserialization of value types (structs) to and from JSON using customizable options.
+/// </summary>
 public class JsonStructSerializer<T> where T : struct
 {
     private readonly JsonSerializerOptions options;
     
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonStructSerializer{T}"/> with default options (camel case, indented, enum as string).
+    /// </summary>
     public JsonStructSerializer()
     {
         options = new()
@@ -16,8 +22,39 @@ public class JsonStructSerializer<T> where T : struct
             Converters = { new JsonStringEnumConverter() }
         };
     }
-    
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonStructSerializer{T}"/> with a custom naming policy and indentation option.
+    /// </summary>
+    /// <param name="namingPolicy">The property naming policy to use.</param>
+    /// <param name="writeIndented">Whether to write indented JSON.</param>
+    public JsonStructSerializer(JsonNamingPolicy namingPolicy, bool writeIndented = true)
+    {
+        options = new()
+        {
+            PropertyNamingPolicy = namingPolicy,
+            WriteIndented = writeIndented,
+            Converters = { new JsonStringEnumConverter() }
+        };
+    }
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonStructSerializer{T}"/> with custom <see cref="JsonSerializerOptions"/>.
+    /// </summary>
+    /// <param name="options">The serializer options to use.</param>
+    public JsonStructSerializer(JsonSerializerOptions options)
+    {
+        this.options = options;
+    }
+    /// <summary>
+    /// Serializes the specified value type to a JSON string.
+    /// </summary>
+    /// <param name="enemy">The value type to serialize.</param>
+    /// <returns>A JSON string representation of the value type.</returns>
     public string Serialize(T enemy) => JsonSerializer.Serialize(enemy, options);
 
+    /// <summary>
+    /// Deserializes the specified JSON string to a value type of <typeparamref name="T"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized value type.</returns>
     public T Deserialize(string json) => JsonSerializer.Deserialize<T>(json, options);
 }
