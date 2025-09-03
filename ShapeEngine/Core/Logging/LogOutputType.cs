9642030122419f6c95f60eaeba6c3f254be937bd
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-09-03 16:18:15 +0200

    Docs: Docs improved.

--- ShapeEngine/Core/Logging/LogOutputType.cs
+++ ShapeEngine/Core/Logging/LogOutputType.cs
@@ -3,6 +3,9 @@ namespace ShapeEngine.Core.Logging;
 /// <summary>
 /// Specifies the output destination for log messages.
 /// </summary>
+/// <remarks>
+/// <see cref="LogOutputType.File"/> and <see cref="LogOutputType.Both"/> need valid file paths to write to a file.
+/// </remarks>
 public enum LogOutputType
 {
     /// <summary>Log messages are written to the console.</summary>
