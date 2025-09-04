65878e8bc4ab92f0f228c8ad5e5655b7fff2182a
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-09-04 16:36:23 +0200

    Feat: SavegameExample scene started. Todo: Explanation for how to implement the example scene added.

---
+++ Examples/Scenes/ExampleScenes/SavegameExample.cs
@@ -0,0 +1,24 @@
+namespace Examples.Scenes.ExampleScenes;
+
+
+
+// Todo:
+// - This scene should be able to generate something, then save it to a savegame, then load it back from the savegame.
+// - It should be able to reset the savegame as well.
+// - Maybe it has multiple savegame slots as well (to demonstrate that functionality).
+// - For instance:
+//   - Generate a random number in a random positioned / sized rect with a random color
+//   - The user can:
+//      - regenerate the number
+//      - drag the top left corner to move the rect
+//      - drag the bottom right corner to resize the rect
+//      - regnerate a new random color
+//      - save the current state to a savegame slot
+//      - load the current state from a savegame slot
+//      - reset the savegame slot
+//      - switch between savegame slots
+
+public class SavegameExample : ExampleScene
+{
+    
+}
