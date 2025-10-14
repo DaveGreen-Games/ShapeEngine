9cf84fccb95db3a8762686540378f95c868f7cc8
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-10-14 17:03:11 +0200

    Refactor: Private ColliderRegister class started for clean up.

--- ShapeEngine/Geometry/CollisionSystem/BroadphaseDynamicSpatialHash.cs
+++ ShapeEngine/Geometry/CollisionSystem/BroadphaseDynamicSpatialHash.cs
@@ -39,6 +39,52 @@ public class BroadphaseDynamicSpatialHash : IBroadphase
             return X == other.X && Y == other.Y;
         }
     }
+
+    private class ColliderRegister<T>
+    {
+        private readonly Dictionary<Collider, HashSet<T>> register = new();
+        private readonly HashSet<Collider> unusedRegisterColliders = [];
+
+        public HashSet<T>? GetEntrySet(Collider collider, int capacity)
+        {
+            HashSet<T> registerSet;
+            if (register.TryGetValue(collider, out var value))
+            {
+                //already added this frame
+                if (!unusedRegisterColliders.Contains(collider))
+                {
+                    return null;
+                }
+                registerSet = value;
+                registerSet.Clear();//clean up from last frame
+                unusedRegisterColliders.Remove(collider);
+            }
+            else
+            {
+                registerSet = new HashSet<T>(capacity);
+                register[collider] = registerSet;
+            }
+            return registerSet;
+        }
+        public void CleanRegister()
+        {
+            //remaining colliders that were not used this frame. Remove them from the register.
+            foreach (var collider in unusedRegisterColliders)
+            {
+                register.Remove(collider);
+            }
+            //set up for next frame
+            unusedRegisterColliders.Clear();
+            //all keys in register are now candidates for removal next frame if not used again
+            unusedRegisterColliders.UnionWith(register.Keys);
+        }
+        public void Close()
+        {
+            register.Clear();
+            unusedRegisterColliders.Clear();
+        }
+    }
+    
     
     private Rect currentBounds = new();//calculated from added colliders
     
