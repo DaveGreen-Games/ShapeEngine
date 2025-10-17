f583c1cc795c21fe7afdd52660e72dfbc4184a1d
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-10-17 16:56:23 +0200

    Docs: Xml summaries added to BroadphaseColliderRegister.

--- ShapeEngine/Geometry/CollisionSystem/BroadphaseColliderRegister.cs
+++ ShapeEngine/Geometry/CollisionSystem/BroadphaseColliderRegister.cs
@@ -1,11 +1,30 @@
 namespace ShapeEngine.Geometry.CollisionSystem;
 
-//TODO: Docs
+/// <summary>
+/// Manages a register of colliders and their associated data for broadphase collision detection.
+/// </summary>
 public class BroadphaseColliderRegister<T>
 {
+    /// <summary>
+    /// Stores the mapping between <see cref="Collider"/> instances and their associated sets of type <typeparamref name="T"/>.
+    /// Used for broadphase collision detection.
+    /// </summary>
     private readonly Dictionary<Collider, HashSet<T>> register = new();
+    
+    /// <summary>
+    /// Tracks colliders that were not used in the current frame and are candidates for removal from the register.
+    /// </summary>
     private readonly HashSet<Collider> unusedRegisterColliders = [];
-
+    
+    /// <summary>
+    /// Adds a new entry for the specified <paramref name="collider"/> to the register or resets an existing entry.
+    /// If the collider was already added this frame, returns null. Otherwise, returns a cleared or new <see cref="HashSet{T}"/> with the specified <paramref name="capacity"/>.
+    /// </summary>
+    /// <param name="collider">The collider to add or reset in the register.</param>
+    /// <param name="capacity">The initial capacity for the associated <see cref="HashSet{T}"/>.</param>
+    /// <returns>
+    /// The <see cref="HashSet{T}"/> associated with the collider, or null if the collider was already added this frame.
+    /// </returns>
     public HashSet<T>? AddEntry(Collider collider, int capacity)
     {
         HashSet<T> registerSet;
@@ -27,15 +46,22 @@ public class BroadphaseColliderRegister<T>
         }
         return registerSet;
     }
-    // public HashSet<T>? GetEntry(Collider collider)
-    // {
-    //     return register.TryGetValue(collider, out var registerBuckets) ? registerBuckets : null;
-    // }
-
+    /// <summary>
+    /// Attempts to retrieve the <see cref="HashSet{T}"/> associated with the specified <paramref name="collider"/>.
+    /// </summary>
+    /// <param name="collider">The collider whose entry is to be retrieved.</param>
+    /// <param name="value">When this method returns, contains the <see cref="HashSet{T}"/> associated with the collider, if found; otherwise, null.</param>
+    /// <returns>
+    /// True if the entry exists in the register; otherwise, false.
+    /// </returns>
     public bool TryGetEntry(Collider collider, out HashSet<T>? value)
     {
         return register.TryGetValue(collider, out value);
     }
+    /// <summary>
+    /// Cleans up the register by removing colliders that were not used in the current frame,
+    /// and prepares the register for the next frame by marking all remaining colliders as unused.
+    /// </summary>
     public void Clean()
     {
         //remaining colliders that were not used this frame. Remove them from the register.
@@ -48,6 +74,9 @@ public class BroadphaseColliderRegister<T>
         //all keys in register are now candidates for removal next frame if not used again
         unusedRegisterColliders.UnionWith(register.Keys);
     }
+    /// <summary>
+    /// Clears all entries from the register and unused colliders, effectively closing and resetting the register.
+    /// </summary>
     public void Close()
     {
         register.Clear();
