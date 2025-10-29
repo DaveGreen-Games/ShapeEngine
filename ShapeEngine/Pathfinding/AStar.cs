720582d1e586be7430ec11f46e05b69ff8691ced
Dave Green <34277803+SoloByte@users.noreply.github.com>
2025-10-29 17:12:57 +0100

    Refactor: Convert AStar class to instance-based and add asynchronous pathfinding method

--- ShapeEngine/Pathfinding/AStar.cs
+++ ShapeEngine/Pathfinding/AStar.cs
@@ -5,13 +5,25 @@ namespace ShapeEngine.Pathfinding;
 /// <summary>
 /// Implements the A* pathfinding algorithm for nodes in a graph.
 /// </summary>
-internal static class AStar
+internal class AStar
 {
-    private static readonly NodeQueue openSet = new(1024);
-    private static readonly HashSet<Node> openSetCells = new(1024);
-    private static readonly HashSet<Node> closedSet = new(1024);
-    private static readonly Dictionary<Node, Node> cellPath = new(1024);
+    private readonly NodeQueue openSet;
+    private readonly HashSet<Node> openSetCells;
+    private readonly HashSet<Node> closedSet;
+    private readonly Dictionary<Node, Node> cellPath;
     
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AStar"/> class.
+    /// </summary>
+    /// <param name="capacity">Estimated number of nodes the algorithm will handle.
+    /// Used to size internal collections to reduce allocations and improve performance.</param>
+    public AStar(int capacity)
+    {
+        openSet = new NodeQueue(capacity);
+        openSetCells = new HashSet<Node>(capacity);
+        closedSet = new HashSet<Node>(capacity);
+        cellPath = new Dictionary<Node, Node>(capacity);
+    }
     
     /// <summary>
     /// Finds a path between two nodes using the A* algorithm for a given layer.
@@ -20,7 +32,7 @@ internal static class AStar
     /// <param name="endNode">The ending node.</param>
     /// <param name="layer">The layer to consider for traversability and weights.</param>
     /// <returns>A <see cref="Path"/> if a path is found; otherwise, null.</returns>
-    public static Path? GetPath(Node startNode, Node endNode, uint layer)
+    public  Path? GetPath(Node startNode, Node endNode, uint layer)
     {
         cellPath.Clear();
         closedSet.Clear();
@@ -125,9 +137,9 @@ internal static class AStar
     /// <param name="from">The end node.</param>
     /// <param name="capacityEstimate">Estimated capacity for the path list.</param>
     /// <returns>A list of rectangles representing the path.</returns>
-    private static List<Rect> ReconstructPath(Node from, int capacityEstimate)
+    private List<Rect> ReconstructPath(Node from, int capacityEstimate)
     {
-        
+        //TODO: Optimize allocations here (DPA issue)
         List<Rect> nodes = new(capacityEstimate) { from.GetRect() };
 
         var current = from;
@@ -146,5 +158,108 @@ internal static class AStar
         nodes.Reverse();
         return nodes;
     }
-  
+    /// <summary>
+    /// Finds a path between two nodes using the A* algorithm for a given layer asynchronously.
+    /// </summary>
+    /// <param name="startNode">The starting node.</param>
+    /// <param name="endNode">The ending node.</param>
+    /// <param name="layer">The layer to consider for traversability and weights.</param>
+    /// <param name="cancellationToken">Optional cancellation token.</param>
+    /// <returns>A <see cref="Path"/> if a path is found; otherwise, null.</returns>
+    public async Task<Path?> GetPathAsync(Node startNode, Node endNode, uint layer, CancellationToken cancellationToken = default)
+    {
+        return await Task.Run(() =>
+        {
+            cellPath.Clear();
+            closedSet.Clear();
+            openSetCells.Clear();
+            openSet.Clear();
+
+            startNode.GScore = 0;
+            startNode.H = startNode.DistanceToTarget(endNode);
+            startNode.FScore = startNode.H;
+            openSet.Enqueue(startNode);
+            openSetCells.Add(startNode);
+
+            while (openSet.Count > 0)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var current = openSet.Dequeue();
+                if(current == null) continue;
+
+                if (current == endNode)
+                {
+                    var countEstimate = startNode.EstimateCellCount(endNode);
+                    var pathPoints = ReconstructPath(current, countEstimate);
+
+                    return new Path(startNode.GetPosition(), endNode.GetPosition(), pathPoints);
+                }
+
+                openSetCells.Remove(current);
+                closedSet.Add(current);
+
+                if (current.Neighbors != null)
+                {
+                    foreach (var neighbor in current.Neighbors)
+                    {
+                        if(closedSet.Contains(neighbor) || !neighbor.IsTraversable(layer)) continue;
+
+                        float tentativeGScore = current.GScore + current.WeightedDistanceToNeighbor(neighbor, layer);
+
+                        if (openSetCells.Contains(neighbor))
+                        {
+                            if (tentativeGScore < neighbor.GScore)
+                            {
+                                neighbor.GScore = tentativeGScore;
+                                neighbor.FScore = neighbor.GScore + neighbor.H;
+                                cellPath[neighbor] = current;
+                            }
+                        }
+                        else
+                        {
+                            neighbor.GScore = tentativeGScore;
+                            neighbor.H = neighbor.DistanceToTarget(endNode);
+                            neighbor.FScore = neighbor.GScore + neighbor.H;
+                            openSet.Enqueue(neighbor);
+                            openSetCells.Add(neighbor);
+                            cellPath[neighbor] = current;
+                        }
+                    }
+                }
+
+                if (current.Connections != null)
+                {
+                    foreach (var connection in current.Connections)
+                    {
+                        if(closedSet.Contains(connection) || !connection.IsTraversable(layer)) continue;
+
+                        float tentativeGScore = current.GScore + current.WeightedDistanceToNeighbor(connection, layer);
+
+                        if (openSetCells.Contains(connection))
+                        {
+                            if (tentativeGScore < connection.GScore)
+                            {
+                                connection.GScore = tentativeGScore;
+                                connection.FScore = connection.GScore + connection.H;
+                                cellPath[connection] = current;
+                            }
+                        }
+                        else
+                        {
+                            connection.GScore = tentativeGScore;
+                            connection.H = connection.DistanceToTarget(endNode);
+                            connection.FScore = connection.GScore + connection.H;
+                            openSet.Enqueue(connection);
+                            openSetCells.Add(connection);
+                            cellPath[connection] = current;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }, cancellationToken);
+    }
+    
 }
