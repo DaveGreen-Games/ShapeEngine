a464c71d0dba98cc1e0f0a20d430b20a0187cba2
Dave Green <34277803+SoloByte@users.noreply.github.com>
2026-06-13 13:46:59 +0200

    Docs: Xml summary added to SimpleStat.cs for better documentation.

--- ShapeEngine/Stats/SimpleStat.cs
+++ ShapeEngine/Stats/SimpleStat.cs
@@ -1,41 +1,129 @@
 namespace ShapeEngine.Stats;
 
-//TODO: Review and make sure everything is in order.
+/// <summary>
+/// Represents a simple stat with a base value and modifiers that affect it.
+/// </summary>
 public class SimpleStat
 {
+    /// <summary>
+    /// Duration value that represents a permanent modifier (never expires).
+    /// </summary>
     public const float PermanentDuration = -1f;
 
+    /// <summary>
+    /// Represents a modifier applied to a stat.
+    /// </summary>
     private struct Modifier
     {
+        /// <summary>
+        /// Unique identifier for the modifier.
+        /// </summary>
         public int Id;
+
+        /// <summary>
+        /// The modifier value.
+        /// </summary>
         public float Value;
+
+        /// <summary>
+        /// The total duration of the modifier.
+        /// </summary>
         public float Duration;
+
+        /// <summary>
+        /// The remaining duration of the modifier.
+        /// </summary>
         public float Remaining;
+
+        /// <summary>
+        /// The number of times the modifier is stacked.
+        /// </summary>
         public int Stacks;
     }
 
+    /// <summary>
+    /// Array of flat bonus modifiers.
+    /// </summary>
     private Modifier[] flatBonuses;
+
+    /// <summary>
+    /// Array of additive multiplier modifiers.
+    /// </summary>
     private Modifier[] additiveMultipliers;
+
+    /// <summary>
+    /// Array of multiplicative multiplier modifiers.
+    /// </summary>
     private Modifier[] multiplicativeMultipliers;
 
+    /// <summary>
+    /// Number of flat bonus modifiers currently applied.
+    /// </summary>
     private int flatBonusCount;
+
+    /// <summary>
+    /// Number of additive multiplier modifiers currently applied.
+    /// </summary>
     private int additiveMultiplierCount;
+
+    /// <summary>
+    /// Number of multiplicative multiplier modifiers currently applied.
+    /// </summary>
     private int multiplicativeMultiplierCount;
 
+    /// <summary>
+    /// The base value of the stat.
+    /// </summary>
     private float baseValue;
+
+    /// <summary>
+    /// The current calculated value of the stat after applying all modifiers and bounds.
+    /// </summary>
     private float currentValue;
+
+    /// <summary>
+    /// The minimum value bound for the stat (if set).
+    /// </summary>
     private float minValue;
+
+    /// <summary>
+    /// The maximum value bound for the stat (if set).
+    /// </summary>
     private float maxValue;
+
+    /// <summary>
+    /// True if the stat has a minimum value bound set.
+    /// </summary>
     private bool hasMinValue;
+
+    /// <summary>
+    /// True if the stat has a maximum value bound set.
+    /// </summary>
     private bool hasMaxValue;
 
+    /// <summary>
+    /// The display name of the stat.
+    /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    /// The abbreviated display name of the stat.
+    /// </summary>
     public string Abbreviation { get; set; }
+
+    /// <summary>
+    /// The description of the stat.
+    /// </summary>
     public string Description { get; set; }
 
+    /// <summary>
+    /// Gets or sets the base value of the stat. Setting this will recalculate the current value.
+    /// </summary>
     public float BaseValue
     {
+        
         get => baseValue;
+        
         set
         {
             baseValue = value;
@@ -43,17 +131,56 @@ public class SimpleStat
         }
     }
 
+    /// <summary>
+    /// Gets the current calculated value of the stat.
+    /// </summary>
     public float CurrentValue => currentValue;
 
+    /// <summary>
+    /// Gets a value indicating whether the stat has a minimum value bound set.
+    /// </summary>
     public bool HasMinValue => hasMinValue;
+
+    /// <summary>
+    /// Gets a value indicating whether the stat has a maximum value bound set.
+    /// </summary>
     public bool HasMaxValue => hasMaxValue;
+
+    /// <summary>
+    /// Gets the minimum value bound of the stat.
+    /// </summary>
     public float MinValue => minValue;
+
+    /// <summary>
+    /// Gets the maximum value bound of the stat.
+    /// </summary>
     public float MaxValue => maxValue;
 
+    /// <summary>
+    /// Gets the number of flat bonus modifiers currently applied.
+    /// </summary>
     public int FlatBonusCount => flatBonusCount;
+
+    /// <summary>
+    /// Gets the number of additive multiplier modifiers currently applied.
+    /// </summary>
     public int AdditiveMultiplierCount => additiveMultiplierCount;
+
+    /// <summary>
+    /// Gets the number of multiplicative multiplier modifiers currently applied.
+    /// </summary>
     public int MultiplicativeMultiplierCount => multiplicativeMultiplierCount;
 
+    /// <summary>
+    /// Creates a new simple stat with the specified base value and optional modifiers.
+    /// </summary>
+    /// <param name="baseValue">The base value of the stat.</param>
+    /// <param name="name">The display name of the stat.</param>
+    /// <param name="abbreviation">The abbreviated display name of the stat.</param>
+    /// <param name="description">The description of the stat.</param>
+    /// <param name="flatBonusCapacity">The initial capacity for flat bonus modifiers (default 4).</param>
+    /// <param name="additiveMultiplierCapacity">The initial capacity for additive multiplier modifiers (default 4).</param>
+    /// <param name="multiplicativeMultiplierCapacity">The initial capacity for multiplicative multiplier modifiers (default 4).</param>
     public SimpleStat(
         float baseValue = 0f,
         string name = "",
@@ -75,6 +202,9 @@ public class SimpleStat
         currentValue = ApplyBounds(baseValue);
     }
 
+    /// <summary>
+    /// Creates a copy of this simple stat.
+    /// </summary>
     private SimpleStat(SimpleStat other)
     {
         baseValue = other.baseValue;
@@ -101,6 +231,10 @@ public class SimpleStat
         CopyModifiers(other.multiplicativeMultipliers, multiplicativeMultipliers, multiplicativeMultiplierCount);
     }
 
+    /// <summary>
+    /// Sets the minimum value bound for the stat. If the current maximum is lower than this value, the maximum is also updated to match.
+    /// </summary>
+    /// <param name="value">The minimum value to set.</param>
     public void SetMin(float value)
     {
         minValue = value;
@@ -114,6 +248,10 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Sets the maximum value bound for the stat. If the current minimum is higher than this value, the minimum is also updated to match.
+    /// </summary>
+    /// <param name="value">The maximum value to set.</param>
     public void SetMax(float value)
     {
         maxValue = value;
@@ -127,6 +265,11 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Sets both minimum and maximum value bounds for the stat.
+    /// </summary>
+    /// <param name="min">The minimum value bound.</param>
+    /// <param name="max">The maximum value bound.</param>
     public void SetBounds(float min, float max)
     {
         if (max < min)
@@ -141,18 +284,27 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Clears the minimum value bound.
+    /// </summary>
     public void ClearMin()
     {
         hasMinValue = false;
         Recalculate();
     }
 
+    /// <summary>
+    /// Clears the maximum value bound.
+    /// </summary>
     public void ClearMax()
     {
         hasMaxValue = false;
         Recalculate();
     }
 
+    /// <summary>
+    /// Clears both minimum and maximum value bounds.
+    /// </summary>
     public void ClearBounds()
     {
         hasMinValue = false;
@@ -160,51 +312,109 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Adds a flat modifier to the stat.
+    /// </summary>
+    /// <param name="value">The flat value to add.</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddFlat(float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref flatBonuses, ref flatBonusCount, 0, value, duration, stacks, false);
     }
 
+    /// <summary>
+    /// Adds a flat modifier to the stat with a specific id.
+    /// </summary>
+    /// <param name="id">The unique identifier for the modifier.</param>
+    /// <param name="value">The flat value to add.</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddFlat(int id, float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref flatBonuses, ref flatBonusCount, id, value, duration, stacks, true);
     }
 
+    /// <summary>
+    /// Adds an additive percentage modifier to the stat.
+    /// </summary>
+    /// <param name="value">The percentage multiplier to add (e.g., 0.25 for +25%).</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddAdditiveMultiplier(float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref additiveMultipliers, ref additiveMultiplierCount, 0, value, duration, stacks, false);
     }
 
+    /// <summary>
+    /// Adds an additive percentage modifier to the stat with a specific id.
+    /// </summary>
+    /// <param name="id">The unique identifier for the modifier.</param>
+    /// <param name="value">The percentage multiplier to add (e.g., 0.25 for +25%).</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddAdditiveMultiplier(int id, float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref additiveMultipliers, ref additiveMultiplierCount, id, value, duration, stacks, true);
     }
 
+    /// <summary>
+    /// Adds a multiplicative percentage modifier to the stat.
+    /// </summary>
+    /// <param name="value">The multiplier to add (e.g., 0.25 for x1.25).</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddMultiplicativeMultiplier(float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref multiplicativeMultipliers, ref multiplicativeMultiplierCount, 0, value, duration, stacks, false);
     }
 
+    /// <summary>
+    /// Adds a multiplicative percentage modifier to the stat with a specific id.
+    /// </summary>
+    /// <param name="id">The unique identifier for the modifier.</param>
+    /// <param name="value">The multiplier to add (e.g., 0.25 for x1.25).</param>
+    /// <param name="duration">The duration of the modifier. Use <see cref="PermanentDuration"/> for a permanent modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
     public void AddMultiplicativeMultiplier(int id, float value, float duration = PermanentDuration, int stacks = 1)
     {
         AddModifier(ref multiplicativeMultipliers, ref multiplicativeMultiplierCount, id, value, duration, stacks, true);
     }
 
+    /// <summary>
+    /// Removes a flat modifier by id if it exists.
+    /// </summary>
+    /// <param name="id">The unique identifier of the modifier to remove.</param>
+    /// <returns>True if a modifier with the given id was removed.</returns>
     public bool RemoveFlat(int id)
     {
         return RemoveModifier(flatBonuses, ref flatBonusCount, id);
     }
 
+    /// <summary>
+    /// Removes an additive multiplier by id if it exists.
+    /// </summary>
+    /// <param name="id">The unique identifier of the modifier to remove.</param>
+    /// <returns>True if a modifier with the given id was removed.</returns>
     public bool RemoveAdditiveMultiplier(int id)
     {
         return RemoveModifier(additiveMultipliers, ref additiveMultiplierCount, id);
     }
 
+    /// <summary>
+    /// Removes a multiplicative multiplier by id if it exists.
+    /// </summary>
+    /// <param name="id">The unique identifier of the modifier to remove.</param>
+    /// <returns>True if a modifier with the given id was removed.</returns>
     public bool RemoveMultiplicativeMultiplier(int id)
     {
         return RemoveModifier(multiplicativeMultipliers, ref multiplicativeMultiplierCount, id);
     }
 
+    /// <summary>
+    /// Updates timed modifiers by reducing their remaining duration.
+    /// </summary>
+    /// <param name="dt">The time delta since the last update.</param>
     public void Update(float dt)
     {
         if (dt <= 0f)
@@ -222,6 +432,9 @@ public class SimpleStat
         }
     }
 
+    /// <summary>
+    /// Resets all modifiers to their initial state (removes all modifiers).
+    /// </summary>
     public void Reset()
     {
         flatBonusCount = 0;
@@ -230,11 +443,18 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Creates a copy of this simple stat.
+    /// </summary>
+    /// <returns>A new SimpleStat instance with the same base value and modifiers.</returns>
     public SimpleStat Copy()
     {
         return new SimpleStat(this);
     }
 
+    /// <summary>
+    /// Recalculates the current value of the stat based on the base value and all modifiers.
+    /// </summary>
     public void Recalculate()
     {
         var value = baseValue + SumModifiers(flatBonuses, flatBonusCount);
@@ -253,6 +473,16 @@ public class SimpleStat
         currentValue = ApplyBounds(value);
     }
 
+    /// <summary>
+    /// Adds or updates a modifier in the specified modifier array.
+    /// </summary>
+    /// <param name="modifiers">The array to add the modifier to.</param>
+    /// <param name="count">The current count of modifiers in the array.</param>
+    /// <param name="id">The unique identifier for the modifier. Use 0 for auto-generated id.</param>
+    /// <param name="value">The value of the modifier.</param>
+    /// <param name="duration">The duration of the modifier.</param>
+    /// <param name="stacks">The number of times to stack the modifier.</param>
+    /// <param name="useId">If true, tries to find and update an existing modifier with the given id instead of adding a new one.</param>
     private void AddModifier(ref Modifier[] modifiers, ref int count, int id, float value, float duration, int stacks, bool useId)
     {
         if (stacks <= 0)
@@ -302,6 +532,13 @@ public class SimpleStat
         Recalculate();
     }
 
+    /// <summary>
+    /// Removes a modifier from the specified array by id if it exists.
+    /// </summary>
+    /// <param name="modifiers">The array to search for the modifier.</param>
+    /// <param name="count">The count of modifiers in the array.</param>
+    /// <param name="id">The unique identifier of the modifier to remove.</param>
+    /// <returns>True if a modifier with the given id was found and removed.</returns>
     private bool RemoveModifier(Modifier[] modifiers, ref int count, int id)
     {
         for (var i = 0; i < count; i++)
@@ -319,6 +556,13 @@ public class SimpleStat
         return false;
     }
 
+    /// <summary>
+    /// Updates all timed modifiers by reducing their remaining duration by dt seconds.
+    /// </summary>
+    /// <param name="modifiers">The array of modifiers to update.</param>
+    /// <param name="count">The count of active modifiers in the array.</param>
+    /// <param name="dt">The time delta since the last update.</param>
+    /// <returns>True if any modifiers expired or changed.</returns>
     private static bool UpdateModifiers(Modifier[] modifiers, ref int count, float dt)
     {
         var changed = false;
@@ -344,6 +588,12 @@ public class SimpleStat
         return changed;
     }
 
+    /// <summary>
+    /// Removes the modifier at the specified index from the array.
+    /// </summary>
+    /// <param name="modifiers">The array to remove from.</param>
+    /// <param name="count">The count of active modifiers in the array.</param>
+    /// <param name="index">The index of the modifier to remove.</param>
     private static void RemoveAt(Modifier[] modifiers, ref int count, int index)
     {
         count--;
@@ -356,6 +606,12 @@ public class SimpleStat
         modifiers[count] = default;
     }
 
+    /// <summary>
+    /// Calculates the sum of all modifiers in the array.
+    /// </summary>
+    /// <param name="modifiers">The array of modifiers.</param>
+    /// <param name="count">The count of active modifiers in the array.</param>
+    /// <returns>The sum of all active modifiers.</returns>
     private static float SumModifiers(Modifier[] modifiers, int count)
     {
         var sum = 0f;
@@ -368,6 +624,11 @@ public class SimpleStat
         return sum;
     }
 
+    /// <summary>
+    /// Applies the minimum and maximum value bounds to the given value.
+    /// </summary>
+    /// <param name="value">The value to apply bounds to.</param>
+    /// <returns>The value clamped within the bounds (or unchanged if no bounds are set).</returns>
     private float ApplyBounds(float value)
     {
         if (hasMinValue && value < minValue)
@@ -383,6 +644,11 @@ public class SimpleStat
         return value;
     }
 
+    /// <summary>
+    /// Ensures the modifier array has sufficient capacity.
+    /// </summary>
+    /// <param name="modifiers">The modifier array to resize if necessary.</param>
+    /// <param name="capacity">The required capacity.</param>
     private static void EnsureCapacity(ref Modifier[] modifiers, int capacity)
     {
         if (modifiers.Length >= capacity)
@@ -402,6 +668,12 @@ public class SimpleStat
         modifiers = newModifiers;
     }
 
+    /// <summary>
+    /// Copies the first count modifiers from the source array to the destination array.
+    /// </summary>
+    /// <param name="source">The source array to copy from.</param>
+    /// <param name="destination">The destination array to copy to.</param>
+    /// <param name="count">The number of modifiers to copy.</param>
     private static void CopyModifiers(Modifier[] source, Modifier[] destination, int count)
     {
         for (var i = 0; i < count; i++)
